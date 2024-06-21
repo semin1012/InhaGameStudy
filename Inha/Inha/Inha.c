7fc3cf82d44892926be966b77cba95f6ac31e169
semin1012 <semin1012@gmail.com>
2024-06-21 15:31:12 +0900

    3일차 Q4. Factorial 계산

--- Inha/Inha/Inha.c
+++ Inha/Inha/Inha.c
@@ -2,35 +2,19 @@
 #include <stdio.h>
 
 /*
-Q4. 키보드로 수식을 입력하면 계산 결과를 출력하는
-	프로그램을 작성하세요. 정수 사칙연산만 입력합니다. 
+Q4. Factorial 계산... 어떤 양의 정수 n을 입력받아서
+	n! 을 구하는 프로그램을 작성하라.
+	ex) 3! -> 3 * 2 * 1  ==> 6
 */
 
 int main()
 {
-	int a, b;
-	double result = 0;
-	char c;
-	printf("사칙연산 입력(정수): ");
-	scanf("%d%c%d", &a, &c, &b);
-	switch (c) {
-	case '+':
-		result = a + b;
-		break;
-	case '-':
-		result = a - b;
-		break;
-	case '*':
-		result = a * b;
-		break;
-	case '/':
-		result = (double)a / b;
-		break;
-	default:
-		printf("Error\n");
-		return 0;
+	int n, result;
+	scanf("%d", &n);
+	result = n;
+	while (n > 1) {
+		n--;
+		result *= n;
 	}
-	if ( c != '/')
-		printf("%d%c%d = %d", a, c, b, (int)result);
-	else printf("%d%c%d = %.2lf", a, c, b, result);
+	printf("%d\n", result);
 }
