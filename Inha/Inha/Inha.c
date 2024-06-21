1d592120680980a7d7d1256f7c83d0eefe2dffd1
semin1012 <semin1012@gmail.com>
2024-06-21 10:29:07 +0900

    세 개의 정수 중 최댓값 구하기

--- Inha/Inha/Inha.c
+++ Inha/Inha/Inha.c
@@ -8,27 +8,9 @@ Q1. 사용자로부터 정수 3개를 입력 받아서 저장을 하고
 */
 
 void main()
-{	/*
-	// 예제 4-8
-	int a = 20, b = 3;
-	double res;
-
-	res = ((double)a) / ((double)b);
-	printf("a = %d, b = %d\n", a, b);
-	printf("a / b 의 결과: %.1lf\n", res);
-
-	a = (int)res;
-	printf("(int) %.1lf의 결과: %d\n", res, a); 
-
-	int a = 10;
-	double b = 3.4;
-
-	// 예제 4-9
-	printf("int형 변수의 크기: %d\n", sizeof(a));
-	printf("double형 변수의 크기: %d\n", sizeof(b));
-	printf("정수형 상수의 크기: %d\n", sizeof 10);
-	printf("수식 결괏값의 크기: %d\n", sizeof(1.5 + 3.4));
-	printf("char 자료형의 크기: %d\n", sizeof(char));
-	*/
-
+{	
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+	int max = (((a > b) ? a : b) > c) ? ((a > b) ? a : b) : c;
+	printf("%d\n", max);
 }
