81f237583950b38dde15c6cfd47c74b18099a6b4
semin1012 <semin1012@gmail.com>
2024-06-25 17:34:15 +0900

    백준 2504번 너무어려워요 괄호쌍

--- Inha/C++ Project/main.cpp
+++ Inha/C++ Project/main.cpp
@@ -8,27 +8,65 @@ int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	int t;
-	cin >> t;
-	while (t--) 
+	stack<char> s;
+	int answer = 0;
+	int temp = 1;
+	bool result = true;
+	char prev = ' ';
+
+	string str;
+	cin >> str;
+
+	for (auto c : str)
 	{
-		string str;
-		stack<char> s;
-		cin >> str;
-		bool result = true;
-		for (auto c : str)
+		if (c == '(' || c == '[')
 		{
-			if (c == '(') s.push(c);
-			else {
-				if (s.empty()) {
-					result = false;
-					break;
-				}
-				if (s.top() == '(') s.pop();				
+			s.push(c);
+			if (c == '(')
+				temp *= 2;
+			else temp *= 3;
+		}
+		else if (c == ')')
+		{
+			if (s.empty() || s.top() != '(')
+			{
+				result = false;
+				break;
+			}
+			s.pop();
+			if (prev == '(')
+			{
+				answer += temp;
+				temp /= 2;
+			}
+			else 
+			{
+				temp /= 2;
 			}
 		}
-		
-		if (result == false || !s.empty()) cout << "NO\n";
-		else cout << "YES\n";
+		else if (c == ']')
+		{
+			if (s.empty() || s.top() != '[') 
+			{
+				result = false;
+				break;
+			}
+			s.pop();
+			if (prev == '[')
+			{
+				answer += temp;
+				temp /= 3;
+			}
+			else
+			{
+				temp /= 3;
+			}
+		}
+		prev = c;
 	}
+	if (!s.empty()) result = false;
+	// 하 이거때문이엇네
+	if (result != false)
+		cout << answer;
+	else cout << "0";
 }
