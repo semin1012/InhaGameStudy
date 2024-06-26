0f4cc44262a9c11aa4dce58b314c0bb095347117
semin1012 <semin1012@gmail.com>
2024-06-26 11:30:11 +0900

    6일차 Q1. 배열 입력받아 최대/최소값

--- Inha/Inha/Inha.c
+++ Inha/Inha/Inha.c
@@ -1,47 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdbool.h>
+#include <limits.h> // MAX, MIN 값을 자동으로 출력해주는 헤더
 
 /*
-// 예제 8-1
-int main()
-{
-	int ary[5];
-
-	ary[0] = 10;
-	ary[1] = 20;
-	ary[2] = ary[0] + ary[1];
-	scanf("%d", &ary[3]);
-
-	printf("%d\n", ary[2]);
-	printf("%d\n", ary[3]);
-	printf("%d\n", ary[4]);
-}
+Q1. 길이가 5인 int형 배열을 선언하고,
+	사용자로부터 5개의 정수를 입력받아
+	다음의 내용을 출력하는 프로그램을 작성하라.
+	1. 입력된 정수 중 최대값
+	2. 입력된 정수 중 최소값
+	3. 입력된 정수들의 총 합
 */
-// 예제 8-2
+
 int main()
 {
-	int score[5];
-	int i;
-	int total = 0;
-	double avg;
+	int arr[5];
+	int max = INT_MIN, min = INT_MAX, sum = 0;
 
-	for (i = 0; i < 5; i++)
+	for (int i = 0; i < 5; i++)
 	{
-		scanf("%d", &score[i]);
+		printf("정수를 입력하세요: ");
+		scanf("%d", &arr[i]);
+		if (max < arr[i])
+			max = arr[i];
+		if (min > arr[i])
+			min = arr[i];
+		sum += arr[i];
 	}
-	for (i = 0; i < 5; i++)
-	{
-		total += score[i];
-	}
-	avg = total / 5.0;
-	for (i = 0; i < 5; i++)
-	{
-		printf("%5d ", score[i]);
-	}
-	printf("\n");
-
-	printf("평균: %.1lf\n", avg);
-
-	return 0;
+	printf("\n입력된 정수 중 최대값: %d\n", max);
+	printf("입력된 정수 중 최소값: %d\n", min);
+	printf("입력된 정수들의 총합: %d\n", sum);
 }
