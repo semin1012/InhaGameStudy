0e0d800e7a210356c99ea44eb928a9ffd9791781
semin1012 <semin1012@gmail.com>
2024-06-27 14:44:13 +0900

    7일차 Q1. 광고판 포인터로 만들기

--- Inha/Inha/Inha.c
+++ Inha/Inha/Inha.c
@@ -7,36 +7,60 @@
 #include <conio.h>
 
 /*
-p. 289 미니 정렬 프로그램
+Q1. 문자열을 입력받아서 광고판에 광고 문자가 출력되는
+	프로그램을 출력하라.
+	문자열 흘러가듯이 출력되는 형태로 구현하라.
+	ex) -> Hello
+	> Hello
+	> ello H
+	> llo He
+	조건 1. 최대 127자까지 입력 가능하다.
+	조건 2. 광고판의 크기는 최대 20자까지 출력 가능하다.
+
+	<< 포인터를 사용해 보자! >>
 
 */
-void swap(double* pa, double* pb);
-void line_up(double* maxp, double* midp, double* minp);
+
+#define MAX 128
+
+void init(char *str, int *length, int *j)
+{
+	printf("내용을 입력하세요: ");
+	gets(str);
+
+	*length = strlen(str);
+	str[*length] = ' ';
+	str[*length + 1] = '\0';
+	*j = 0;
+}
 
 int main()
 {
-	double max, mid, min;
+	char str[MAX];
+	int length = 0, j = 0;
+	char* temp = &str;
+	
+	init(str, &length, &j);
 
-	printf("실수값 3개 입력: ");
-	scanf("%lf%lf%lf", &max, &mid, &min);
-	line_up(&max, &mid, &min);
-	printf("정렬된 값 출력: %.1lf, %.1lf, %.1lf\n", max, mid, min);
-}
+	while (1)
+	{
+		system("cls");
 
+		for (int i = 0; i < 20; i++) {
+			printf("%c", temp[(i+j)%(length+1)]);
+		}
+		j++;
 
-void swap(double* pa, double* pb)
-{
-	double temp;
+		printf("\n\n문자를 입력하면 내용을 다시 입력합니다.\n");
+		Sleep(300);
 
-	temp = *pa;
-	*pa = *pb;
-	*pb = temp;
-}
+		if (_kbhit())   // 아무키나 눌려지면 멈춥니다
+		{
+			system("cls");
+			for (int i = 0; i < MAX; i++) 
+				str[i] = '\0';
 
-void line_up(double* maxp, double* midp, double* minp)
-{
-	if (*maxp < *midp) swap(maxp, midp);
-	if (*maxp < *minp) swap(maxp, minp);
-
-	if (*minp > *midp) swap(minp, midp);
+			init(str, &length, &j);
+		}
+	}
 }
