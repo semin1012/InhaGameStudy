3602d4823013d4525fcb35032abad9064127967c
semin1012 <semin1012@gmail.com>
2024-06-28 09:55:34 +0900

    8일차 챕터 11. 예제

--- Inha/C++ Project/main.cpp
+++ Inha/C++ Project/main.cpp
@@ -1,72 +1,41 @@
 #include <iostream>
-#include <stack>
+#include <queue>
 #include <string>
 #include <algorithm>
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	stack<char> s;
-	int answer = 0;
-	int temp = 1;
-	bool result = true;
-	char prev = ' ';
-
-	string str;
-	cin >> str;
-
-	for (auto c : str)
-	{
-		if (c == '(' || c == '[')
-		{
-			s.push(c);
-			if (c == '(')
-				temp *= 2;
-			else temp *= 3;
-		}
-		else if (c == ')')
-		{
-			if (s.empty() || s.top() != '(')
-			{
-				result = false;
-				break;
-			}
-			s.pop();
-			if (prev == '(')
-			{
-				answer += temp;
-				temp /= 2;
-			}
-			else 
-			{
-				temp /= 2;
-			}
-		}
-		else if (c == ']')
-		{
-			if (s.empty() || s.top() != '[') 
-			{
-				result = false;
-				break;
-			}
-			s.pop();
-			if (prev == '[')
-			{
-				answer += temp;
-				temp /= 3;
-			}
-			else
-			{
-				temp /= 3;
-			}
-		}
-		prev = c;
-	}
-	if (!s.empty()) result = false;
-	// 하 이거때문이엇네
-	if (result != false)
-		cout << answer;
-	else cout << "0";
+using namespace std;
+#define X first
+#define Y second // pair에서 first, second를 줄여서 쓰기 위해서 사용
+int board[502][502] =
+{ 
+ {1,1,1,0,1,0,0,0,0,0},
+ {1,0,0,0,1,0,0,0,0,0},
+ {1,1,1,0,1,0,0,0,0,0},
+ {1,1,0,0,1,0,0,0,0,0},
+ {0,1,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,0,0,0} }; // 1이 파란 칸, 0이 빨간 칸에 대응
+bool vis[502][502]; // 해당 칸을 방문했는지 여부를 저장
+int n = 7, m = 10; // n = 행의 수, m = 열의 수
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 }; // 상하좌우 네 방향을 의미
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    queue<pair<int, int> > Q;
+    vis[0][0] = 1; // (0, 0)을 방문했다고 명시
+    Q.push({ 0,0 }); // 큐에 시작점인 (0, 0)을 삽입.
+    while (!Q.empty()) {
+        pair<int, int> cur = Q.front(); Q.pop();
+        cout << '(' << cur.X << ", " << cur.Y << ") -> ";
+        for (int dir = 0; dir < 4; dir++) { // 상하좌우 칸을 살펴볼 것이다.
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir]; // nx, ny에 dir에서 정한 방향의 인접한 칸의 좌표가 들어감
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue; // 범위 밖일 경우 넘어감
+            if (vis[nx][ny] || board[nx][ny] != 1) continue; // 이미 방문한 칸이거나 파란 칸이 아닐 경우
+            vis[nx][ny] = 1; // (nx, ny)를 방문했다고 명시
+            Q.push({ nx,ny });
+        }
+    }
 }
--- Inha/Inha/Inha.c
+++ Inha/Inha/Inha.c
@@ -1,80 +1,60 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdbool.h>
-#include <limits.h> // MAX, MIN 값을 자동으로 출력해주는 헤더
+#include <stdbool.h>	// C언어에서의 bool 헤더
+#include <limits.h>		// MAX, MIN 값을 자동으로 출력해주는 헤더
 #include <string.h>
-#include <Windows.h>
-#include <conio.h>
+#include <Windows.h>	// system("cls")
+#include <conio.h>		// Sleep(1000)
+
 
 /*
-Q1. 문자열을 입력받아서 광고판에 광고 문자가 출력되는
-	프로그램을 출력하라.
-	문자열 흘러가듯이 출력되는 형태로 구현하라.
-	ex) -> Hello
-	> Hello
-	> ello H
-	> llo He
-	조건 1. 최대 127자까지 입력 가능하다.
-	조건 2. 광고판의 크기는 최대 20자까지 출력 가능하다.
+// 예제 11-4 
+int main()
+{
+	char ch;
 
-	<< 포인터를 사용해 보자! >>
+	//int i;
+	//for (i = 0; i < 3; i++)
+	//{
+	//	scanf("%c", &ch);
+	//	printf("%c", ch);
+	//}
 
-Q2. 길이가 6인 int형 배열을 선언하고, 1,2,3,4,5,6 으로 초기화한 다음 
-	배열에 저장된 값의 순서가 반대로 6,5,4,3,2,1 이 되도록 변경하는
-	프로그램을 작성하라.
-	포인터를 활용하라.
-
-Q3. 임의의 문자열을 입력받아 뒤집어 출력하는 프로그램을 작성하라.
-	ex > 
-		Input	> Hello World 
-		Output	> dlroW olleH
-
-p.320. 로또 번호 생성 프로그램 (입력 후 출력)
+	while (1)
+	{
+		scanf("%c", &ch);
+		if (ch == '\n')
+			break;
+		printf("%c", ch);
+	}
+}
 */
 
-void input_nums(int *lotto_nums);
-void print_nums(int* lotto_nums);
+
+void my_gets(char* str, int size);
 
 int main()
 {
-	int lotto_nums[6];
-
-	input_nums(lotto_nums);
-	print_nums(lotto_nums);
-}
-
-void input_nums(int* lotto_nums)
-{
-	int n = 0;
-	int input = 0;
-	bool check = false;
-	while (n != 6) {
-		printf("번호 입력: ");
-		scanf("%d", &input);
-
-		for (int i = 0; i < n; i++) 
-		{
-			if ( *(lotto_nums+i) == input ) 
-			{
-				printf("같은 번호가 있습니다!\n");
-				check = true;
-				break;
-			}
-		}
-		if (check == true)
-		{
-			check = false;
-			continue;
-		}
-		lotto_nums[n] = input;
-
-		n++;
+	char str[7];
+	
+	while (1)
+	{
+		my_gets(str, sizeof(str));
+		printf("입력한 문자열: %s\n", str);
 	}
 }
 
-void print_nums(int* lotto_nums)
+void my_gets(char* str, int size)
 {
-	printf("로또 번호: ");
-	for (int i = 0; i < 6; i++)
-		printf("%2d ", lotto_nums[i]);
+	int ch;
+	int i = 0;
+
+	ch = getchar();
+	while ((ch != '\n') && (i < size - 1))
+	{
+		str[i] = ch;
+		i++;
+		ch = getchar();
+	}
+	str[i] = '\0';
 }
