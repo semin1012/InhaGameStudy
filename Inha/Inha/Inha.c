224a37ed2ccd0b4ba81da16a13f255232eb48abb
semin1012 <semin1012@gmail.com>
2024-07-03 14:13:47 +0900

    10일차 Q2. 이중 포인터

--- Inha/Inha/Inha.c
+++ Inha/Inha/Inha.c
@@ -22,86 +22,61 @@ Q1. 야구 게임
 	3 > 2 6 4 -> 1s 1b (2의 위치가 맞으나 6은 위치가 다름)
 	4 > 2 1 6 -> HR 
 
-// 예제 14-?
-int main()
-{
-	int ary1[4] = { 1, 2, 3, 4 };
-	int ary2[4] = { 11, 12, 13, 14 };
-	int ary3[4] = { 21, 22, 23, 24 };
-	int* pary[3] = { ary1, ary2, ary3, };
-	int i, j;
-	
-	int maxJ = sizeof(ary1[0]);
-	int maxI = sizeof(pary) / maxJ;
-
-	for (i = 0; i < maxI; i++)
-	{
-		for (j = 0; j < maxJ; j++)
-		{
-			printf("%5d", pary[i][j]);
-		}
-		printf("\n");
-	}
-
-	printf("\n-----------------주소-----------------\n\n");
-
-	for (i = 0; i < 3; i++)
-	{
-		printf("pary[%d]: %p, %x\n", i, pary[i], pary[i]);
-		printf("ary1[%d]: %p, %x\n", i, &ary1[i], &ary1[i]);
-		printf("\n");
-	}
-}
-// 예제 15-1
-int main()
-{
-	int a = 10;
-	int* pi;
-	int** ppi;
-
-	pi = &a;
-	ppi = &pi;
-
-	printf("----------------------------------------------------\n");
-	printf("  변수    변수값      &연산      *연산      **연산 \n");
-	printf("----------------------------------------------------\n");
-	printf("  a%12d%12u\n", a, &a);
-	printf(" pi%12u%12u%12d\n", pi, &pi, *pi);
-	printf("ppi%12u%12u%12u%12u\n", ppi, &ppi, *ppi, **ppi);
-}
-// 예제 15-2
-void swap_ptr(char** ppa, char** ppb);
-
-int main()
-{
-	char* pa = "success";
-	char* pb = "failure";
-
-	printf("pa -> %s, pb -> %s\n", pa, pb);
-	swap_ptr(&pa, &pb);
-	printf("pa -> %s, pb -> %s\n", pa, pb);
-}
-
-void swap_ptr(char** ppa, char** ppb)
-{
-	char* pt;
-	pt = *ppa;
-	*ppa = *ppb;
-	*ppb = pt;
-}
-// 예제 15-4
-int main()
-{
-	int ary[5];
-	printf("  ary의 값: %u\t", ary);
-	printf("ary의 주소: %u\n", &ary);
-	printf("   ary + 1: %u\t", ary + 1);
-	printf("  &ary + 1: %u\n", &ary + 1);
-}
+Q2. 다음과 같이 두 개의 int형 포인터 변수와 길이가 5인 
+	int형 배열을 선언한다.
+	int *maxPtr;
+	int *minPtr;
+	int arr[5];
+	MaxAndMin이라는 함수를 정의하고 이를 호출해서 위 배열과 
+	두 포인터를 변수로 전달한다.
+	이 함수의 호출이 완료되면 
+	포인터 변수 maxPtr에는 가장 큰 배열 요소의 주소값이 
+	포인터 변수 minPtr에는 가장 작은 배열 요소의 주소값이
+	저장되어야 한다. 
+	arr 주소값, 최대/최소의 주소값 
+	maxPtr, minPtr이 가진 주소에 저장된 값도 출력하라.
 */
-// 배열 포인터
+
+#define MAX_ARRAY 5
+void MaxAndMin(int **maxPtr, int **minPtr);
+
 int main()
 {
-	int* pAry[5];
-	int(*pa)[4];	//배열 포인터
+	int arr[MAX_ARRAY];
+	int* maxPtr = &arr;
+	int* minPtr = &arr;
+
+	printf("정수를 5개 입력하세요: ");
+	for (int i = 0; i < MAX_ARRAY; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	MaxAndMin(&maxPtr, &minPtr);
+
+	printf("   arr의 주소: %12u\n", &arr);
+	printf("최대값의 주소: %12u, maxPtr의 값: %d\n", maxPtr, *maxPtr);
+	printf("최소값의 주소: %12u, minPtr의 값: %d\n", minPtr, *minPtr);
+}
+
+void MaxAndMin(int** maxPtr, int** minPtr)
+{
+	int* temp = *maxPtr;
+
+	// 최대값 찾기
+	for (int i = 0; i < MAX_ARRAY; i++) 
+	{
+		if (*(*(maxPtr)+i) > *temp)
+			temp = *(maxPtr)+i;
+	}
+	*maxPtr = temp;
+
+	temp = *minPtr;
+	// 최소값 찾기
+	for (int i = 0; i < MAX_ARRAY; i++) 
+	{
+		if (*(*(minPtr)+i) < *temp)
+			temp = *(minPtr)+i;
+	}
+	*minPtr = temp;
 }
