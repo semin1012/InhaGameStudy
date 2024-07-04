929a7a196f1bf1fa4733ee9a8451c149d4c427d9
semin1012 <semin1012@gmail.com>
2024-07-04 14:20:51 +0900

    11일차 Q1. 단어 단위 뒤집기

--- Inha/Inha/Inha.c
+++ Inha/Inha/Inha.c
@@ -7,106 +7,78 @@
 #include <conio.h>		// Sleep(1000)
 
 /*
-// 예제 16-1
-int main()
-{
-	int* pi;
-	double* pd;
+Q1. 사용자로부터 문자열을 입력 받아서
+	단어 단위로 역순으로 출력하는 프로그램을
+	작성하라. "Quit"가 입력될 때까지 반복한다.
 
-	pi = (int*)malloc(sizeof(int) * 1000000000);
-	pd = (double*)malloc(sizeof(double));
-
-	for (int i = 0; i < 1000000000; i++) {
-		if (pi[i] == NULL)
-		{
-			printf("i = %d\n", i);
-			printf("# 메모리가 부족합니다.\n");
-			exit(1);
-		}
-
-
-		pi[i] = i;
-		*pd = 3.4;
-
-		//printf("정수형으로 사용: %d\n", pi[i]);
-		//printf("실수형으로 사용: %.1lf\n", *pd);
-
-		//printf("\n");
-	}
-
-	printf("완료\n");
-	free(pi);
-	free(pd);
-}
-// 예제 16-3
-int main()
-{
-	int* pi;
-	int size = 5;
-	int count = 0;
-	int num;
-	int i;
-
-	pi = (int*)calloc(size, sizeof(int));
-	while (1)
-	{
-		printf("양수만 입력하세요 => ");
-		scanf("%d", &num);
-		if (num <= 0) break;
-		if (count == size)
-		{
-			size += 5;
-			pi = (int*)realloc(pi, size * sizeof(int));
-			printf("size 변경, 현재 size: %d\n", size);
-		}
-		pi[count++] = num;
-	}
-	for (i = 0; i < count; i++)
-	{
-		printf("%5d", pi[i]);
-	}
-	free(pi);
-}
-// 예제 16-5 수정
-void print_str(char** ps);
-
-int main()
-{
-	char temp[80];
-	char* str;
-	int size = 5;
-	str = (char*)calloc(size, sizeof(char));
-	int count = 0;
-	
-	while (1)
-	{
-		// 안되는디? 이중 포인터 써볼것
-		printf("문자열을 입력하세요. (end는 종료): ");
-		gets(temp);
-		if (strcmp(temp, "end") == 0) break;
-		if (count + 1 == size)
-		{
-			size += 5;
-			str = (char*)realloc(str, sizeof(char) * size);
-			printf("size 변화, size 크기: %d\n", size);
-		}
-		str[count] = (char*)malloc(strlen(temp) + 1);
-		strcpy(str[count++], temp);
-	}
-
-	print_str(str);
-	for (int i = 0; str[i] != NULL; i++)
-	{
-		free(str[i]);
-	}
-}
-
-void print_str(char** ps)
-{
-	while (*ps != NULL)
-	{
-		printf("%s\n", *ps);
-		ps++;
-	}
-}
+	ex>
+		입력 > I am happy
+		출력 > happy am I
+		메모리 할당, 잘라서 카피(개수 알아야 함) 
 */
+
+int main()
+{
+	char str[80];
+	// char* pstr[20] = { 0 };
+	char temp[80] = { 0 };
+	int count = 0;
+	int size = 2;
+	char** pstr = (char**)calloc(size, sizeof(char));
+	int i = 0;
+	int j = 0;
+
+	while (1)
+	{
+		printf("> ");
+		gets(str);
+		
+		if (strcmp(str, "Quit") == 0) break;
+
+		while(str[i] != '\0') {
+			temp[j] = str[i];
+			j++;
+			i++;
+			// 띄어쓰기 나오면
+			if (str[i] == ' ') {
+				if (size == count) {
+					size += 5;
+					printf("  size 변경, 현재 size: %d\n", size);
+					pstr = (char**)realloc(pstr, size * sizeof(char));
+				}
+
+				temp[j] = '\0';
+				pstr[count] = (char*)malloc(j + 1);
+				// temp 내용을 pstr[count]에 저장
+				strcpy(pstr[count], temp);
+				count++;
+				j = 0;
+				i++; // 공백 넘어가기 위함
+			}
+		}
+
+		// temp의 마지막 단어도 pstr[count]에 넣어주기
+		temp[j] = '\0';
+		pstr[count] = (char*)malloc(j + 1);
+		strcpy(pstr[count], temp);
+
+
+		// 바꾼 거 출력
+		printf("> ");
+		for (int i = 0; i <= count; i++)
+		{
+			printf("%s ", pstr[count - i]);
+		}
+		printf("\n  count: % d, size : % d\n", count, size);
+		printf("\n");
+
+		// 초기화
+		count = 0;
+		j = 0;
+		i = 0;
+	}
+	for (int i = 0; pstr[i] != NULL ; i++)
+	{
+		free(pstr[i]);
+	}
+}
