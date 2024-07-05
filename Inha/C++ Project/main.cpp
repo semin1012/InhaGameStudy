d7e1b93d360423098443b8adbc45f4ccb3464ebc
semin1012 <semin1012@gmail.com>
2024-07-05 14:31:19 +0900

    12일차 p.540 성적 처리 프로그램

--- Inha/C++ Project/main.cpp
+++ Inha/C++ Project/main.cpp
@@ -4,60 +4,62 @@
 #include <algorithm>
 using namespace std;
 
-int board[51][51];
-int visit[51][51];
+char board[102][102];
+bool visit[102][102];
+int dir_x[4] = { 0, 0, 1, -1 };
+int dir_y[4] = { 1, -1, 0, 0 };
 
-int dirX[4] = { 0, 0, 1, -1 };
-int dirY[4] = { 1, -1, 0, 0 };
+int bfs(int n)
+{
+	int result= 0;
+	queue<pair<int, int>> q;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (visit[i][j] == false)
+			{
+				q.push({ j, i });
+				result++;
+				visit[i][j] = true;
+				while (!q.empty()) {
+					auto cur = q.front(); q.pop();
+
+					for (int i = 0; i < 4; i++) {
+						int dx = cur.first + dir_x[i];
+						int dy = cur.second + dir_y[i];
+
+						if (dx < 0 || dy < 0 || dx >= n || dy >= n) continue;
+						if (board[i][j] != board[dy][dx]) continue;
+						if (visit[dy][dx] == true ) continue;
+
+						visit[dy][dx] = true;
+						q.push({ dx, dy });
+					}
+				}
+			}
+		}
+	}
+
+	return result;
+}
+
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	int t, m, n, k;
-	int x, y;
-	cin >> t;
-	int result = 0;
-	while (t--)
-	{
-		cin >> m >> n >> k;
-		for (int i = 0; i < k; i++) {
-			cin >> x >> y;
-			board[y][x] = 1;
-		}
-	
-		queue<pair<int, int>> q;
 
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				if (board[i][j] == 1 && visit[i][j] == 0)
-				{
-					result++;
-					q.push({ i, j });
-					while (!q.empty() )
-					{
-						auto cur = q.front();
-						q.pop();
-						for (int i = 0; i < 4; i++) {
-							int dx = cur.first + dirX[i];
-							int dy = cur.second + dirY[i];
+	int n;
+	cin >> n;
 
-							if (dx < 0 || dy < 0 || dx >= n || dy >= m) continue;
-							if (board[dx][dy] != 1 || visit[dx][dy] != 0) continue;
-							visit[dx][dy] = 1;
-							q.push({ dx, dy });
-						}
-					}
-				}
-			}
-		}
-		cout << result << '\n';
-		result = 0;
-		for (int i = 0; i < n; i++) {
-			fill(board[i], board[i] + m, 0);
-			fill(visit[i], visit[i] + m, 0);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j];
 		}
 	}
+
+	int result = bfs(n);
+
+
+	cout << result << endl;
 }
--- Inha/Inha/Inha.c
+++ Inha/Inha/Inha.c
@@ -1,245 +1,226 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdbool.h>	// C언어에서의 bool 헤더
-#include <limits.h>		// MAX, MIN 값을 자동으로 출력해주는 헤더
 #include <string.h>
 #include <Windows.h>	// system("cls")
-#include <conio.h>		// Sleep(1000)
+#include <crtdbg.h>		// 메모리 릭이 일어났는지 확인하기 위함
 
 /*
-Q1. 사용자로부터 문자열을 입력 받아서
-	단어 단위로 역순으로 출력하는 프로그램을
-	작성하라. "Quit"가 입력될 때까지 반복한다.
+Q1. p.540 성적 처리 프로그램
+	학번, 이름, 학점(국/영/수)을 입력받을 수 있는
+	구조체를 만들고, 임의 수의 학생에 대한 데이터를 입력받아
+	출력하는 프로그램을 작성하라.
+	다음 4개의 메뉴를 구성하고, 각 메뉴의 선택시 작동은 다음과 같다.
 
-	ex>
-		입력 > I am happy
-		출력 > happy am I
-		메모리 할당, 잘라서 카피(개수 알아야 함) 
-
-p.502 소수 계산 프로그램
-
+	1. 입력 - 학번, 이름, 성적 순으로 입력
+	2. 출력 - 성적(평균)에 따라 (정렬된 형태로) 출력
+	3. 검색 - 이름으로 해당 학생의 학점, 성적을 출력
+	4. 종료
+	조건>
+		1. 번호 중복 불가, 이름은 중복 가능
+		2. 검색 -> 이름 검색으로 하고 먼저 찾은 것을 우선 출력
+		3. 메모리 할당 활용, 자기 참조 구조체 활용
+		4. 입력/출력/검색은 각각 개별 함수로 구현한다.
 */
 
-int main(int argc, char** argv)
+struct Student
 {
-	printf(" >>> arguments <<< \n");
-	printf(" >>> argv[1] : %s \n", argv[1]);
-	//printf(" >>> argv[1] : %s \n", argv[1]);
-
-	int num = atoi(argv[1]);
-	// calloc으로 고쳤고
-	char** a = (char**)calloc(num+1, sizeof(char*));
-	
-	bool decimal = true;
-	int col = 1;
-	char temp[10] = { 0 };
-
-	// i는 0부터 되게 범위 고쳤고
-	for (int i = 0; i < num; i++)
-	{
-		decimal = true;
-		if (i < 2) decimal = false;
-		for (int j = 2; j < i; j++)
-		{
-			if (i % j == 0) {
-				decimal = false;
-				break;
-			}
-		}
-		int t = i, count = 1;
-		while ( t != 0 )
-		{
-			t = t / 10;
-			count++;
-		}
-
-		a[i] = (char*)malloc(count*sizeof(char*));
-
-		if (decimal == true) {
-			_itoa(i, a[i], 10);
-		}
-		else {
-			strcpy(a[i], "X");
-		}
-	}
-
-	for (int i = 0; i < num; i++)
-	{
-		if (a[i] != NULL) {
-			printf("%5s", a[i]);
-			if (col % 5 == 0)
-				printf("\n");
-			col++;
-		}
-	}
-
-	for (int i = 0; a[i] != NULL; i++) {
-		free(a[i]);
-	}
-}
-
-/*
-//예제 17-2
-
-struct profile
-{
+	int studentNum;
 	char name[20];
-	int age;
-	double height;
-	char* intro;
-};
-
-int main()
-{
-	struct profile yuni;
-
-	strcpy(yuni.name, "서하윤");
-	yuni.age = 17;
-	yuni.height = 164.5;
-
-	yuni.intro = (char*)malloc(80);
-	printf("자기소개 : ");
-	gets(yuni.intro);
-
-	printf("이름: %s\n", yuni.name);
-	printf("나이: %d\n", yuni.age);
-	printf("키: %.1lf\n", yuni.height);
-	printf("자기소개: %s\n", yuni.intro);
-	free(yuni.intro);
-}
-// 예제 17-3
-struct profile
-{
-	int age;
-	double height; 
-};
-
-struct student
-{
-	struct profile pf;
-	int id;
-	double grade;
-};
-
-int main()
-{
-	struct student yuni;
-
-	yuni.pf.age = 17;
-	yuni.pf.height = 164.5;
-	yuni.id = 315;
-	yuni.grade = 4.3;
-
-	printf("나이: %d\n", yuni.pf.age);
-	printf("키: %.1lf\n", yuni.pf.height);
-	printf("학번: %d\n", yuni.id);
-	printf("학점: %.1lf\n", yuni.grade);
-}
-// 예제 17-4
-struct student
-{
-	int id;
-	char name[20];
-	double grade;
-};
-
-int main()
-{
-	struct student s1 = { 315, "홍길동", 2.4 },
-				   s2 = { 316, "이순신", 3.6 },
-				   s3 = { 317, "세종대왕", 4.4 };
-
-	struct student max;
-
-	max = s1;
-	if (s2.grade > max.grade) max = s2;
-	if (s3.grade > max.grade) max = s3;
-
-	printf("학번: %d\n", max.id);
-	printf("이름: %s\n", max.name);
-	printf("학점: %.1lf\n", max.grade);
-}
-// 예제 17-6
-struct score
-{
 	int kor;
 	int eng;
 	int math;
+	double avg;
 };
 
-int main()
+struct List
 {
-	struct score yuni = { 90, 80, 70 };
-	struct score* ps = &yuni;
+	struct Student student;
+	struct List* next;
+	struct List* prev;
+};
 
-	printf("국어: %d\n", (*ps).kor);
-	printf("영어: %d\n", ps->eng);
-	printf("수학: %d\n", ps->math);
+void printMenu()
+{
+	printf("=================================================\n");
+	printf("1. 입력 - 학번, 이름, 성적 순으로 입력\n");
+	printf("2. 출력 - 성적(평균)에 따라 정렬된 형태로 출력\n");
+	printf("3. 검색 - 이름으로 해당 학생의 학점, 성적을 출력\n");
+	printf("4. 종료\n");
+	printf("=================================================\n");
 }
-// 예제 17-9
-struct list
+
+void inputMenu(int* menu)
 {
-	int num;
-	struct list* next;
-};
+	printf("> ");
+	scanf("%d", menu);
+}
 
-int main()
+void inputStudent(struct List* head, struct List* tail)
 {
-	struct list a = { 10, 0 }, b = { 20, 0 }, c = { 30, 0 }, d = { 40, 0 };
-	struct list* head = &a, * current;
-
-	a.next = &b;
-	b.next = &c;
-	c.next = &d;
-
-	printf("head->num : %d\n", head->num);
-	printf("head->next->num : %d\n\n", head->next->num);
-
-	printf("list all: ");
-	current = head;
-	while (current != NULL)
+	while (1)
 	{
-		printf("%d ", current->num);
-		current = current->next;
+		bool dup = false;
+
+		struct List* newNode = (struct List*)malloc(sizeof(struct List));
+		struct List* prevNode = tail->prev;
+
+		printf("학번 이름 학점(국/영/수)를 순서대로 입력하세요(0 입력 시까지): ");
+		scanf("%d", &newNode->student.studentNum);
+
+		if (newNode->student.studentNum == 0) {
+			break;
+		}
+
+		scanf("%s%d", &newNode->student.name, &newNode->student.kor);
+		scanf("%d%d", &newNode->student.eng, &newNode->student.math);
+
+		struct List* node = head->next;
+		while (node != tail)
+		{
+			if (node->student.studentNum == newNode->student.studentNum) {
+				printf("학번이 같습니다. 다시 입력하세요.\n\n");
+				dup = true;
+				break;
+			}
+			node = node->next;
+		}
+		if (dup == true)
+			continue;
+
+
+
+		newNode->student.avg = (newNode->student.kor + newNode->student.eng + newNode->student.math) / 3.f;
+
+		prevNode->next = newNode;
+		newNode->prev = prevNode;
+		newNode->next = tail;
+		tail->prev = newNode;
 	}
-	printf("\n");
-
 }
 
-// 예제 17-10
-union student
+void printStudents(struct List* head, struct List* tail)
 {
-	int num;
-	double grade;
-};
-
-int main()
-{
-	union student s1 = { 315 };
-	printf("학번: %d\n", s1.num);
-	s1.grade = 4.4;
-	printf("학점: %.1lf\n", s1.grade);
-	printf("학번: %d\n", s1.num);
-}
-enum season { SPRING, SUMMER, FALL, WINTER };
-
-int main()
-{
-	enum season ss;
-	char* pc = NULL;
-
-	ss = SPRING;
-	switch (ss)
+	if (head->next == tail)
 	{
-	case SPRING:
-		pc = "inline"; break;
-	case SUMMER:
-		pc = "swimming"; break;
-	case FALL:
-		pc = "trip"; break;
-	case WINTER:
-		pc = "skiing"; break;
+		printf("저장된 학생이 없습니다.\n\n");
+		return;
 	}
-	printf("나의 레저 활동 => %s\n", pc);
+
+	struct List* tempi = head->next;
+	struct List* tempj = head->next;
+
+	while (tempi != tail)
+	{
+		tempj = head->next;
+		while (tempj != tail->prev)
+		{
+			if (tempj->student.avg < tempj->next->student.avg)
+			{
+				struct Student temp;
+				temp = tempj->student;
+				tempj->student = tempj->next->student;
+				tempj->next->student = temp;
+			}
+			tempj = tempj->next;
+		}
+		tempi = tempi->next;
+	}
+
+	// 출력 부분
+	struct List* node = head->next;
+	while (node != tail)
+	{
+		printf("이름: %s, 학번: %d, 평균 성적: %.2lf \n", node->student.name, node->student.studentNum, node->student.avg);
+		node = node->next;
+	}
+
 }
-*/
+
+void searchStudent(struct List* head, struct List* tail)
+{
+	if (head->next == tail)
+	{
+		printf("저장된 학생이 없습니다.\n\n");
+		return;
+	}
+
+	char searchName[20];
+
+	printf("학생의 이름을 입력하세요: ");
+	scanf("%s", searchName);
+
+	struct List* node = head->next;
+	while (node != tail)
+	{
+		if (strcmp(node->student.name, searchName) == 0)
+		{
+			printf("이름: %s \n", node->student.name);
+			printf("  학번: %d\n", node->student.studentNum);
+			printf("  국어 성적: %d\n", node->student.kor);
+			printf("  영어 성적: %d\n", node->student.eng);
+			printf("  수학 성적: %d\n", node->student.math);
+			printf("\n평균 성적: %.2lf\n", node->student.avg);
+			return;
+		}
+		node = node->next;
+	}
+
+	printf("\n찾는 학생이 존재하지 않습니다.\n\n");
+}
+
+int main(int argc, char** argv)
+{
+	int menu;
+
+	struct List* head = (struct List*)malloc(sizeof(struct List));
+	struct List* tail = (struct List*)malloc(sizeof(struct List));
+
+	head->next = tail;
+	head->prev = NULL;
+	tail->next = NULL;
+	tail->prev = head;
+
+	while (1)
+	{
+		printMenu();
+		inputMenu(&menu);
+		
+		switch (menu)
+		{
+		case 1:
+			inputStudent(head, tail);
+			break;
+		case 2:
+			printStudents(head, tail);
+			break;
+		case 3:
+			searchStudent(head, tail);
+			break;
+		case 4:
+			printf("프로그램을 종료합니다.\n\n");
+
+			struct List* node = head;
+			while (node)
+			{
+				struct List* deleteNode = node;
+				node = node->next;
+				free(deleteNode);
+			}
+			/*_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF || _CRTDBG_LEAK_CHECK_DF); // -> F5로 실행하면 메모리 릭 번호 나옴
+			// _CrtSetBreakAlloc(70);			// -> 그 번호를 이 함수 안에 넣으면 어떤 변수인지 보여줌
+
+			int* arr = NULL;
+			arr = (int*)malloc(sizeof(int) * 4);
+
+			_CrtDumpMemoryLeaks();*/
+
+			return 0;
+		default:
+			printf("다시 입력하세요.\n\n");
+		}
+		printf("\n엔터를 입력하세요.\n");
+		_getch();		// 엔터 입력 전까지 안 넘어가게 하기 위함
+		system("cls");
+	}
+}
