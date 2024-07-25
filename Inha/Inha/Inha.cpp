8a8e2fb5ff1ef5cb5af078f60b0c1e04a30b7514
semin1012 <semin1012@gmail.com>
2024-07-25 11:57:52 +0900

    23일차 Update의 동작 변경시키는 방법 두가지, 예제

--- Inha/Inha/Inha.cpp
+++ Inha/Inha/Inha.cpp
@@ -1,6 +1,5 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-/*
-*/
+
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -8,88 +7,71 @@
 using namespace std;
 
 /*
-Q1. 삼각형의 3개의 점 (x1, y1), (x2, y2), (x3, y3)를 알고 있을 때
-	이 삼각형의 면적을 구하는 프로그램을 작성하라.
-	ex > 세 점 입력: 1.5 -3.4 4.6 5 9.5 -3.4 (순서대로 x1,y1~x3,y3)
-		 삼각형의 면적: 33.6
-
-		 헤론의 공식 적용
-
-struct POINT
+// p.406 Listing 07.8
+const int ArSize = 8;
+int sum_arr(const int* begin, const int* end);
+int main()
 {
-	float x, y;
-};
+	int cookies[ArSize] = { 1, 2, 4, 8, 16, 32, 64, 128 };
+
+	int sum = sum_arr(cookies, cookies + ArSize);
+	cout << "먹은 과자 수 합계: " << sum << endl;
+	sum = sum_arr(cookies, cookies + 3);
+	cout << "처음 세 사람은 과자 " << sum << "개를 먹었습니다.\n";
+	sum = sum_arr(cookies + 4, cookies + 8);
+	cout << "마지막 네 사람은 과자 " << sum << "개를 먹었습니다.\n";	// 과자 240개 너무하는거아님?
+	
+	return 0;
+}
+
+int sum_arr(const int* begin, const int* end)
+{
+	const int* pt;
+	int total = 0;
+
+	for (pt = begin; pt != end; pt++)
+		total = total + *pt;
+	
+	return total;
+}
+*/
+
+// case 1. 함수 포인터로 하고 싶은 동작 변경하기
+void (*update)(void);	// 함수 포인터로 쓰면 하고 싶은 내용 변경시키기 유용하다
+void lobby();
+void menu();
 
 int main()
 {
-	POINT t1, t2, t3;
-	float length1, length2, length3, s, a;
-
-	cout << "> 세 점 입력: ";
-	cin >> t1.x >> t1.y >> t2.x >> t2.y >> t3.x >> t3.y;
-
-	length1 = sqrt(pow(t2.x - t1.x, 2) + pow(t2.y - t1.y, 2));
-	length2 = sqrt(pow(t3.x - t2.x, 2) + pow(t3.y - t2.y, 2));
-	length3 = sqrt(pow(t1.x - t3.x, 2) + pow(t1.y - t3.y, 2));
-
-	s = (length1 + length2 + length3) / 2;
-	a = sqrt(s * (s - length1) * (s - length2) * (s - length3));
-	cout << "삼각형의 면적: " << a << endl;
+	update = menu;
+	while (1)
+		update();
+	update = lobby;
 }
-*/
+
+void lobby()
+{
+	update = menu;
+}
+
+void menu()
+{
+	update = lobby;
+}
 
 /*
-Q2. 게임에서 비밀번호에 어떤 규칙을 요구하는 경우가 있다.
-	다음과 같이 비밀번호를 만들어야 한다고 하자.
-	1. 비밀번호는 적어도 8개의 문자여야 한다.
-	2. 비밀번호는 문자와 숫자로만 구성되어야 한다.
-	3. 비밀번호는 적어도 두 개의 숫자가 포함되어야 한다.
-	사용자가 비밀번호를 입력했을 때
-	위 규칙에 맞으면 "Valid Password"를 출력하고
-	그렇지 않으면 "Invalid Password"를 출력하라. 
-	isAlpha 이런 거 써도 된다. 
-*/
-
+// case 2. switch-case 문으로 하고 싶은 동작 변경하기
 int main()
 {
-	string password;
-	int numCnt;
-	bool isValid = true;
-	while (1)
+	int select;
+	switch (select)
 	{
-		cout << "> password: ";
-		cin >> password;
-		numCnt = 0;
-		isValid = true;
-
-		if (password.length() < 8)
-		{
-			cout << "#Invalid Password: 비밀번호는 적어도 8개의 문자여야 합니다.\n\n";
-			continue;
-		}
-
-		
-		for (auto c : password)
-		{
-			if (!isalnum(c))
-			{
-				cout << "#Invalid Password: 비밀번호는 문자와 숫자로만 구성되어야 합니다.\n\n";
-				isValid = false;
-				break;
-			}
-
-			if (isdigit(c)) numCnt++;
-		}
-
-		if (isValid == false) continue;
-
-		if (numCnt < 2)
-		{
-			cout << "#Invalid Password: 비밀번호는 적어도 두 개의 숫자가 포함되어야 합니다.\n\n";
-			continue;
-		}
-
-		cout << "Valid Password\n";
-		return 0;
+	case 1:
+		menu();
+		break;
+	case 2:
+		lobby();
+		break;
 	}
 }
+*/
