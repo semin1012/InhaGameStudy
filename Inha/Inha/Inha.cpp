53fba79d7d1095e95dc52181b942b51649a0437d
semin1012 <semin1012@gmail.com>
2024-07-25 09:47:47 +0900

    23일차 Q1, Q2

--- Inha/Inha/Inha.cpp
+++ Inha/Inha/Inha.cpp
@@ -7,191 +7,91 @@
 #include <math.h>
 using namespace std;
 
-// 방법 1. 2 ~ (n-1)
-// 방법 2. 2 ~ (n-1) 사이의 홀수로만 계산
-// 방법 3. 소수로만 계산
-// 방법 4. 소수로 계산하면서, 자신의 숫자보다 큰 소수는 계산 안 함
-
 /*
-Q1. 소수를 판별하는 프로그램을 작성하고 개선하라.
-	1. 1씩 증가하면서 소수 판단
-	2. 홀수로만 계산하기
-	3. 홀수 + 이전 계산된 소수로 나누어지지 않는 수
-	4. 제곱근 n 이하의 모든 소수로 나누었을 때 나누어 떨어지지 않는 수
-	   + 이전에 계산된 소수로 나누어지지 않는 수
+Q1. 삼각형의 3개의 점 (x1, y1), (x2, y2), (x3, y3)를 알고 있을 때
+	이 삼각형의 면적을 구하는 프로그램을 작성하라.
+	ex > 세 점 입력: 1.5 -3.4 4.6 5 9.5 -3.4 (순서대로 x1,y1~x3,y3)
+		 삼각형의 면적: 33.6
 
-	순서도 그리기 
-	실행한 횟수 카운트 출력 - 각 케이스마다 몇 번을 계산했는지 cnt를 출력
-	각각의 알고리즘에 성능 체크 - 시간 측정, 10만번 정도 돌려 보기
-*/
+		 헤론의 공식 적용
 
-int temp1[500'000] = { 2, 0, }; // 최대 오십만번 계산할 수 있도록 크기 지정, 2는 넣고 시작
-int temp2[500'000] = { 2, 0, };
-
-/*
-	방법 1. 1씩 증가하면서 소수 판단
-	반환: 계산된 횟수 cnt
-*/
-long long firstCase(int n)
+struct POINT
 {
-	long long cnt = 0;
-	bool isDecimal = true;
-	for (int i = 2; i < n; i++)
-	{
-		isDecimal = true;
-		for (int j = 2; j < i; j++)
-		{
-			cnt++;
-			if (i % j == 0) {
-				isDecimal = false;
-				break;
-			}
-		}
-		if (isDecimal == true);
-			//printf("%d ", i);
-	}
-	//printf("\n");
-	return cnt;
-}
-
-/*
-	방법 2. 홀수로만 계산하기 (짝수면 소수 아님)
-	반환: 계산된 횟수 cnt
-*/
-long long secondCase(int n)
-{
-	long long cnt = 0;
-	bool isDecimal = true;
-	//printf("%d ", 2);
-	for (int i = 3; i < n; i+=2)
-	{
-		isDecimal = true;
-		for (int j = 3; j < i; j+=2)
-		{
-			cnt++;
-			if (i % j == 0) {
-				isDecimal = false;
-				break;
-			}
-		}
-		if (isDecimal == true);
-			//printf("%d ", i);
-	}
-	//printf("\n");
-	return cnt;
-}
-
-/*
-	방법 3. 홀수 + 이전 계산된 소수로 나누어지지 않는 수
-	반환: 계산된 횟수 cnt
-*/
-long thirdCase(int n)
-{
-	long cnt = 0;
-	int index = 1;
-	bool isDecimal = true;
-	//printf("%d ", 2);
-	for (int i = 3; i < n; i += 2)
-	{
-		isDecimal = true;
-		for (int j = 0; j < index; j++)
-		{
-			cnt++;
-			if (i % temp1[j] == 0) {
-				isDecimal = false;
-				break;
-			}
-		}
-
-		if (isDecimal == true)
-		{
-			//printf("%d ", i);
-			temp1[index++] = i;
-		}
-	}
-	//printf("\n");
-	return cnt;
-}
-
-/*
-	방법 4. 제곱근 n 이하의 모든 소수로 나누었을 때 나누어 떨어지지 않는 수
-			+ 이전에 계산된 소수로 나누어지지 않는 수
-	반환: 계산된 횟수 cnt
-*/
-long fourthCase(int n)
-{
-	long cnt = 0;
-	int index = 1;
-	int root = sqrt(n);
-	bool isDecimal = true;
-	//printf("%d ", 2);
-	for (int i = 3; i <= n - 1; i+=2)
-	{
-		isDecimal = true;
-		if (i == 2); //printf("%d ", i);
-		if (i % 2 == 0) continue;		// 짝수면 계산 안 하도록
-
-		for (int j = 0; j < index; j++)
-		{
-			cnt++;
-			if (i % temp2[j] == 0) {
-				isDecimal = false;
-				break;
-			}
-		}
-
-		if (isDecimal == true)
-		{
-			//printf("%d ", i);
-			if( i <= root )			// n의 제곱근 이하일 때만 소수 저장
-				temp2[index++] = i;
-		}
-	}
-	//printf("\n");
-	return cnt;
-
-}
+	float x, y;
+};
 
 int main()
 {
-	int n;
-	long long count = 0;
-	clock_t start, end;
-	float res;
-	printf("n 을 입력하세요: ");
-	scanf("%d", &n);
+	POINT t1, t2, t3;
+	float length1, length2, length3, s, a;
 
-	
-	/* case 1 시간 측정 */
-	start = clock();
-	count = firstCase(n);	// 방법 1로 계산
-	end = clock();
-	res = (float)(end - start) / CLOCKS_PER_SEC;
-	printf("case 1:: 소요 시간: %.2f, 계산 횟수: %lld\n", res, count);
+	cout << "> 세 점 입력: ";
+	cin >> t1.x >> t1.y >> t2.x >> t2.y >> t3.x >> t3.y;
 
-	/* case 2 시간 측정 */
-	count = 0;
-	start = clock();
-	count = secondCase(n);	// 방법 2로 계산
-	end = clock();
-	res = (float)(end - start) / CLOCKS_PER_SEC;
-	printf("case 2:: 소요 시간: %.2f, 계산 횟수: %lld\n", res, count);
+	length1 = sqrt(pow(t2.x - t1.x, 2) + pow(t2.y - t1.y, 2));
+	length2 = sqrt(pow(t3.x - t2.x, 2) + pow(t3.y - t2.y, 2));
+	length3 = sqrt(pow(t1.x - t3.x, 2) + pow(t1.y - t3.y, 2));
 
+	s = (length1 + length2 + length3) / 2;
+	a = sqrt(s * (s - length1) * (s - length2) * (s - length3));
+	cout << "삼각형의 면적: " << a << endl;
+}
+*/
 
-	/* case 3 시간 측정 */
-	count = 0;
-	start = clock();
-	count = thirdCase(n);	// 방법 3로 계산
-	end = clock();
-	res = (float)(end - start) / CLOCKS_PER_SEC;
-	printf("case 3:: 소요 시간: %.2f, 계산 횟수: %lld\n", res, count);
+/*
+Q2. 게임에서 비밀번호에 어떤 규칙을 요구하는 경우가 있다.
+	다음과 같이 비밀번호를 만들어야 한다고 하자.
+	1. 비밀번호는 적어도 8개의 문자여야 한다.
+	2. 비밀번호는 문자와 숫자로만 구성되어야 한다.
+	3. 비밀번호는 적어도 두 개의 숫자가 포함되어야 한다.
+	사용자가 비밀번호를 입력했을 때
+	위 규칙에 맞으면 "Valid Password"를 출력하고
+	그렇지 않으면 "Invalid Password"를 출력하라. 
+	isAlpha 이런 거 써도 된다. 
+*/
 
+int main()
+{
+	string password;
+	int numCnt;
+	bool isValid = true;
+	while (1)
+	{
+		cout << "> password: ";
+		cin >> password;
+		numCnt = 0;
+		isValid = true;
 
-	/* case 4 시간 측정 */
-	count = 0;
-	start = clock();
-	count = fourthCase(n);	// 방법 4로 계산
-	end = clock();
-	res = (float)(end - start) / CLOCKS_PER_SEC;
-	printf("case 4:: 소요 시간: %.2f, 계산 횟수: %lld\n", res, count);
+		if (password.length() < 8)
+		{
+			cout << "#Invalid Password: 비밀번호는 적어도 8개의 문자여야 합니다.\n\n";
+			isValid = false;
+			continue;
+		}
+
+		
+		for (auto c : password)
+		{
+			if (!isalnum(c))
+			{
+				cout << "#Invalid Password: 비밀번호는 문자와 숫자로만 구성되어야 합니다.\n\n";
+				isValid = false;
+				break;
+			}
+
+			if (isdigit(c)) numCnt++;
+		}
+
+		if (numCnt < 2)
+		{
+			cout << "#Invalid Password: 비밀번호는 적어도 두 개의 숫자가 포함되어야 합니다.\n\n";
+			isValid = false;
+			continue;
+		}
+
+		if (isValid == false) continue;
+
+		cout << "Valid Password\n";
+		return 0;
+	}
 }
