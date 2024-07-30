ac6e281743b72111fd0e6ba7418f07568f018c4e
semin1012 <semin1012@gmail.com>
2024-07-30 10:21:08 +0900

    25일차 Q1, Q2

--- Inha/Inha/Inha.cpp
+++ Inha/Inha/Inha.cpp
@@ -7,142 +7,101 @@
 using namespace std;
 
 /* 
-Q1. 선형 검색을 하기 위한 기본 순서도와 이를 바탕으로
-	선형 검색 프로그램을 작성컴퓨터 바탕화면 키우기하라.
-	ex > 데이터 입력: 6 5 2 1 8 9 7 4
-	검색 데이터 > 9
-	> 자료 중에 9가 있습니다.
-	검색 데이터 > 3
-	> 자료 중에 3은 없습니다. 
+Q1. 점(x1, y1) 를 입력받아 그 점이 (x0, y0)를 중심으로 하고
+	반지름 r 인 원의 내부에 존재하는지 검사하는 프로그램을 작성하라.
+	ex>
+		input > r : 10, x0 = 0, y0 = 0, x1 = 4, y1 = 5
+		output > 점 (4, 5)는 중점이 (0, 0)이고 반지름이 10인 
+		원 안에 있습니다.
 
-	1. 선형 검색으로만
-	2. p.120 Q2와 같은 형태로 검색 과정을 표시하라.
-	3. 보초법을 이용하여 선형 검색을 수정하고 성능을 비교하라.
+	점이 원 내부에 존재하는지 검사하는 함수를 만들고,
+	점(x1, y1), (x0, y0)를 인자로 넘겨줄 때 포인터 구조체를 
+	이용해서 만들 것
 */
 
 /*
-*/
-#define SIZE 200'000'000
+struct POINT
+{
+	int x, y;
+};
 
-int nums[SIZE + 1];
-
-void LinearSearch(int nums[], int find_num);
-void SentinelLinearSearch(int nums[], int find_num);
+bool isIncluded(POINT* p1, POINT* p2, int* r)
+{
+	float d = sqrt(pow((p2->x - p1->x), 2) + pow((p2->y - p1->y), 2));
+	if (d <= (*r))
+	{
+		return true;
+	}
+	return false;
+}
 
 int main()
 {
-	int find_num;
-	clock_t start, end;
-	float res;
+	int r;
+	POINT p1, p2;
 
-	for (int i = 0; i < SIZE; i++)
-	{
-		nums[i] = i;	// 그냥 0부터 99'999까지 넣는다 
-	}
+	cout << "> r: ";
+	cin >> r;
+	cout << "> x0 y0: ";
+	cin >> p1.x >> p1.y;
+	cout << "> x1 y1: ";
+	cin >> p2.x >> p2.y;
 
-	while (1)
-	{
-		cout << "검색 데이터 > ";
-		cin >> find_num;
-
-		// 1. 일반 선형 검색 
-		start = clock();
-		LinearSearch(nums, find_num);	
-		end = clock();
-		res = (float)(end - start) / CLOCKS_PER_SEC;
-		printf("선형 검색 소요 시간: %.7f\n", res);
-
-
-		// 2. 보초법
-		nums[SIZE] = find_num;	// 보초법
-
-		start = clock();
-		SentinelLinearSearch(nums, find_num);
-		end = clock();
-		res = (float)(end - start) / CLOCKS_PER_SEC;
-		printf("보초법 소요 시간: %.7f\n", res);
-
-		cout << "\n\n";
-	}
+	if (isIncluded(&p1, &p2, &r))
+		cout << "점 (" << p2.x << ", " << p2.y << ")는 중점이 (" << p1.x << ", " << p1.y << ")이고 반지름이 " << r << "인 원 안에 있습니다.\n";
+	else cout << "점 (" << p2.x << ", " << p2.y << ")는 중점이 (" << p1.x << ", " << p1.y << ")이고 반지름이 " << r << "인 원 안에 없습니다.\n";
 }
-
-void LinearSearch(int nums[], int find_num)
-{
-	for (int i = 0; i < SIZE; i++)
-	{
-		if (nums[i] == find_num)
-		{
-			cout << "> 자료 중에 " << find_num << "이(가) 있습니다.\n";
-			return;
-		}
-
-		if (i == SIZE - 1)
-		{
-			cout << "> 자료 중에 " << find_num << "은(는) 없습니다.\n";
-			return;
-		}
-	}
-}
-
-void SentinelLinearSearch(int nums[], int find_num)
-{
-	int i = 0;
-	for (i; i <= SIZE; i++)
-	{
-		if (nums[i] == find_num)
-		{
-			break;
-		}
-	}
-
-	if ( i == SIZE ) cout << "> 자료 중에 " << find_num << "은(는) 없습니다.\n";
-	else cout << "> 자료 중에 " << find_num << "이(가) 있습니다.\n";
-}
+*/
 
 /*
-#define SIZE 8
+Q2. 선1의 두 점 (x1, y1)과 (x2, y2), 선2의 두 점 (x3, y3)과 (x4, y4)로 주어진
+	두 선의 교차점을 구하는 프로그램을 작성하라.
+	크래머 공식을 이용하라. 
 
-void LinearSearch(int nums[], int find_num);
+	ex> 점 4개 입력: x1, y1, x2, y2, x3, y3, x4, y4
+		>> 2 2 5 -1.0 4.0 2.0 -1.0 -2.0
+		>> 교차점은 ( 000, 000 ) 이다. 
+
+		>> 2 2 7 6.0 4.0 2.0 -1.0 -2.0
+		>> 두 직선은 평행이다. 
+*/
+
+struct POINT
+{
+	float x, y;
+};
+
+bool isCrossed(POINT* p1, POINT* p2, POINT* p3, POINT* p4, POINT* result)
+{
+	float temp1, temp2;
+	float a = p2->y - p1->y;
+	float b = p1->x - p2->x;
+	temp1 = a * p1->x + b * p1->y;
+
+	float c = p4->y - p3->y;
+	float d = p3->x - p4->x;
+	temp2 = c * p3->x + d * p3->y;
+
+	float r = (a * d) - (b * c);
+
+	if (r == 0) return false;
+
+	result->x = ((temp1 * d) - (temp2 * b)) / r;
+	result->y = ((temp2 * a) - (temp1 * c)) / r;
+
+	return true;
+}
 
 int main()
 {
-	int nums[SIZE] = { 6, 4, 3, 2, 1, 9, 8, 7 };
-	int find_num;
+	POINT p1, p2, p3, p4;
+	POINT result;
 
-	cout << "> 검색 데이터: ";
-	cin >> find_num;
-	
-	LinearSearch(nums, find_num);
+	cout << "점 4개 입력:  x1, y1, x2, y2, x3, y3, x4, y4\n";
+	cout << ">> ";
+	cin >> p1.x >> p1.y >> p2.x >> p2.y >> p3.x >> p3.y >> p4.x >> p4.y;
+
+	if (isCrossed(&p1, &p2, &p3, &p4, &result))
+		cout << "교차점은 (" << result.x << ", " << result.y << ") 이다.\n";
+	else cout << "두 직선은 평행이다.\n";
 }
-
-void LinearSearch(int nums[], int find_num)
-{
-	for (int i = 0; i < SIZE; i++)
-	{
-		for (int j = 0; j < SIZE; j++)
-		{
-			if (i == j) cout << "* ";
-			else cout << "  ";
-		}
-		cout << "\n";
-
-		for (int j = 0; j < SIZE; j++)
-		{
-			cout << nums[j] << " ";
-		}
-		cout << "\n";
-
-		if (nums[i] == find_num)
-		{
-			cout << find_num << "은(는) nums[" << i  << "]에 존재합니다.\n";
-			return;
-		}
-
-		if (i == SIZE - 1)
-		{
-			cout << "> 자료 중에 " << find_num << "은(는) 없습니다.\n";
-			return;
-		}
-	}
-}
-*/
