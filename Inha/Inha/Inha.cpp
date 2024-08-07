2dbc6cffc895b9e58b741b8fb4a30fb61ed5540b
semin1012 <semin1012@gmail.com>
2024-08-07 12:32:53 +0900

    30일차 Q2. cVector3 클래스

--- Inha/Inha/Inha.cpp
+++ Inha/Inha/Inha.cpp
@@ -15,38 +15,63 @@ Q1. 다음과 같은 형태로 출력하는 프로그램을 작성하라.
 	1	2	4	8	4	2	1
 
 Q2. cVector3 클래스 정의	-> 교과서 3차원으로 바꿀 것
-	
-	static float Dot(cVector3& v1, cVector3& v2);
-	static Vector Cross(cVector3& v1, cVector3& v2);
-	float Length();
-	cVector3 Normalize();
-	static float Angle(cVector3& v1, cVector3& v2);
+		직각좌표계 <-> 극 좌표계 변환하는 함수는 제거
+	함수 추가
+		cVector3 operator/(double n) const;
+		static float Dot(cVector3& v1, cVector3& v2);
+		static cVector3 Cross(cVector3& v1, cVector3& v2);
+		static float Angle(cVector3& v1, cVector3& v2);
+		float Length();
+		cVector3 Normalize();
 
-	cVector3 operator/(double n) const;
+	ex >
+		v1 x, y, z: 1 2 3
+		v2 x, y, z: -4 -5 6
+		v1 length: 3.7xxx 
+		v2 length: 8.7xxx
+		v1 * 3: 3 6 9
+		3 * v1: 3 6 9
+		v2 / 2: -2 -2.5 3
+		v1.Dot(v2) = 4
+		v2.Cross(v2) = 27 -18 3
+		Angle(v1~v2): 83.xxxx
+	ex >
+		v1 -1 3 2
+		v2 3 -4 1
+		v1 + v2 > 2 -1 3
+		v1 - v2 > -4 7 1
+		v1.normalize -> -0.267xxx, 0.801xxx, 0.53xxxx
 */
 
 int main()
 {
-	int n;
-	cin >> n;
-	bool minus = false;
+	using namespace VECTOR;
+	cVector3 v1, v2;
+	cout << " v1 x, y, z: ";
+	cin >> v1;
 
-	for (int i = 0; i <= n; i++)
-	{
-		minus = false;
-		for (int j = 0, k = 1; j <= 2 * n; j++)
-		{
-			if (n - i <= j && n + i >= j)
-			{
-				cout << k << " ";
-				if (k >= pow(2, i) && minus == false) minus = true;
-				if ( minus ) k /= 2;
-				else k *= 2;
-			}
-			else cout << "  ";
+	cout << " v2 x, y, z: ";
+	cin >> v2;
 
-		}
-		cout << '\n';
-	}
+	cout << "v1 length: " << v1.Length() << endl;
+	cout << "v2 length: " << v2.Length() << endl;
+
+	cout << "v1 * 3 = " << v1 * 3 << endl;
+	cout << "3 * v1 = " << 3 * v1 << endl;
+	cout << "v2 / 2 = " << v2 / 2 << endl;
+
+	cout << "v1.dot(v2): " << v1.Dot(v2) << endl;
+	cout << "v1.cross(v2): " << v1.Cross(v2) << endl;
+	cout << "angle(v1~v2): " << v1.Angle(v2) << endl;
+
+	cout << " v1 x, y, z: ";
+	cin >> v1;
+
+	cout << " v2 x, y, z: ";
+	cin >> v2;
+
+	cout << "v1 + v2 > " << v1 + v2 << endl;
+	cout << "v1 - v2 > " << v1 - v2 << endl;
+	cout << "v1.normalize -> " << v1.Normalize() << endl;
 }
 
---
+++ Inha/Inha/cVector3.cpp
@@ -0,0 +1,105 @@
+#include <cmath>
+#include "cVector3.h"
+using std::sqrt;
+using std::sin;
+using std::cos;
+using std::atan;
+using std::atan2;
+using std::cout;
+
+namespace VECTOR
+{
+	const double Rad_to_deg = 45.0 / atan(1.0);
+
+
+	cVector3::cVector3()
+	{
+		x = y = z = 0.0;
+	}
+
+	cVector3::cVector3(const double& n1, const double& n2, const double& n3)
+	{
+		x = n1, y = n2, z = n3;
+	}
+
+	cVector3::~cVector3()
+	{
+	}
+
+	cVector3 cVector3::operator+(const cVector3& b) const
+	{
+		return cVector3(x + b.x, y + b.y, z + b.z);
+	}
+
+	cVector3 cVector3::operator-(const cVector3& b) const
+	{
+		return cVector3(x - b.x, y - b.y, z - b.z);
+	}
+
+	cVector3 cVector3::operator*(double n) const
+	{
+		return cVector3(x * n, y * n, z * n);
+	}
+
+	cVector3 cVector3::operator/(double n) const
+	{
+		return cVector3(x / n, y / n, z / n);
+	}
+
+	cVector3 operator*(double n, const cVector3& a)
+	{
+		return cVector3(a.x * n, a.y * n, a.z * n);
+	}
+
+	std::ostream& operator<<(std::ostream& os, const cVector3& v)
+	{
+		os << v.x << " " << v.y << " " << v.z;
+		return os;
+	}
+
+	std::istream& operator>>(std::istream& is, cVector3& v)
+	{
+		is >> v.x >> v.y >> v.z;
+		return is;
+	}
+
+	double cVector3::Dot(cVector3& v)
+	{
+		return x * v.x + y * v.y + z * v.z;
+	}
+
+	cVector3 cVector3::Cross(cVector3& v)
+	{
+		return cVector3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+	}
+
+	double cVector3::Angle(cVector3& v)
+	{
+		double temp = Dot(v) / (Length() * v.Length());
+		//temp = std::max(-1.0, std::min(1.0, temp));
+		return acos(temp) * (180 / 3.141592);
+	}
+
+	double cVector3::Length()
+	{
+		return sqrt(x * x + y * y + z * z);
+	}
+
+	cVector3 cVector3::Normalize()
+	{
+		return cVector3(x / Length(), y / Length(), z / Length());
+	}
+
+	bool cVector3::operator==(const cVector3& v) const
+	{
+		if (x == v.x && y == v.y && z == v.z) return true;
+		return false;
+	}
+
+	bool cVector3::operator!=(const cVector3& v) const
+	{
+		if (x != v.x || y != v.y || z != v.z) return true;
+		return false;
+	}
+
+}
