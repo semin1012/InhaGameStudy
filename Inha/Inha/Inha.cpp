f4cb58018347d583e2fa14feef99217dae0bf9c8
semin1012 <semin1012@gmail.com>
2024-08-13 17:10:39 +0900

    33일차 어려워요 후위표기법 계산기

--- Inha/Inha/Inha.cpp
+++ Inha/Inha/Inha.cpp
@@ -1,47 +1,139 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include "String.h"
+#include <stack>
+#include <queue>
+#include <string>
 using namespace std;
 
 /*
 Q1. p.889 연습 문제 2
+
+Q2. 후위표기법을 활용하여 스택 계산기 프로그램을 작성하라. 
+	() < +,- < x,/
+	((4x12)x(123+7)
 */
+
+struct OPERATOR
+{
+	string c;
+	int num;
+};
 
 int main()
 {
-	String s1(" and I am a c++ student.");
-	String s2 = "영문 이름을 입력하십시오: ";
-	String s3;
+	string str;
+	stack<string> st;
+	queue<string> q;
+	OPERATOR operators[6] = { {"(", 0}, {")",0}, {"+", 1}, {"-",1}, {"x", 2}, {"/", 2} };
+	bool isOperator = false;
+	string numTemp;
+	int numTempLength = 0;
+	string prevTemp;
+	int prevNum = 0;
 
-	cout << s2;
-	cin >> s3;
-	s2 = "My name is " + s3;
-	cout << s2 << ".\n";
-	s2 = s2 + s1;
-	s2.stringup();
-	cout << "다음 문자열에는\n" << s2 << "\n문자 'A'가 " << s2.has('A') << "개 들어 있습니다.\n";
-	s1 = "red";
-	String rgb[3] = { String(s1), String("green"), String("blue")};
-	cout << "빛의 삼원색의 이름을 하나만 입력하십시오: ";
-	String ans;
-	bool success = false;
-	while (cin >> ans)
+	//str = "(2+2)x(3+3)";
+	cout << "계산식을 입력하세요: \n";
+	cin >> str;
+
+	for (int i = 0; i < str.length(); i++)
 	{
-		ans.stringlow();
-		for (int i = 0; i < 3; i++)
+		isOperator = false;
+
+		for (int j = 0; j < 6; j++)
 		{
-			if (ans == rgb[i])
+			if (operators[j].c[0] == str[i])
 			{
-				cout << "맞았습니다!\n";
-				success = true;
+				isOperator = true;
+
+				if (!st.empty())
+				{
+					if (st.top() == "+" || st.top() == "-")
+						prevNum = 1;
+					else if (st.top() == "x" || st.top() == "/")
+						prevNum = 2;
+				}
+
+				if (numTempLength != 0)
+				{
+					q.push(numTemp);
+					numTempLength = 0;
+					numTemp = "";
+				}
+
+				if (str[i] == '(')
+				{
+					st.push(operators[j].c);
+					break;
+				}
+
+				if (str[i] == ')')
+				{
+					while (st.top() != "(")
+					{
+						q.push(st.top());
+						st.pop();
+					}
+
+					st.pop();	// "(" 삭제
+					break;
+				}
+
+				// 이전 거가 우선순위 더 높다면
+				if (prevNum > operators[j].num && st.top() != "(")
+				{
+					q.push(st.top());
+					st.pop();
+
+					st.push(operators[j].c);
+				}
+				else
+				{
+					st.push(operators[j].c);
+				}
 				break;
 			}
 		}
-		if (success)
-			break;
-		else
-			cout << "다시 입력하십시오: ";
+
+		if (isOperator == false)	// 숫자
+		{
+			numTempLength++;
+			numTemp += str[i];
+		}
 	}
-	cout << "프로그램을 종료합니다.\n";
-	return 0;
+
+	while (!st.empty())
+	{
+		q.push(st.top());
+		st.pop();
+	}
+
+	string top;
+
+	while (!q.empty())
+	{
+		st.push(q.front());
+		q.pop();
+
+		if (st.top() == "x" || st.top() == "+" || st.top() == "-" || st.top() == "/")
+		{
+			top = st.top();
+
+			st.pop();
+			int temp = stoi(st.top());
+			st.pop();
+
+			if (top == "x")
+				temp *= stoi(st.top());
+			else if (top == "+")
+				temp += stoi(st.top());
+			else if (top == "-")
+				temp -= stoi(st.top());
+			else
+				temp /= stoi(st.top());
+			st.pop();
+			st.push(to_string(temp));
+		}
+	}
+
+	cout << "결과: " << st.top() << endl;
 }
