48ad4d282925edcc272b86d8718417d9b266a727
semin1012 <semin1012@gmail.com>
2024-08-15 09:15:19 +0900

    34일차 후위표기법 계산기 수정 필요

--- Inha/Inha/Inha.cpp
+++ Inha/Inha/Inha.cpp
@@ -3,17 +3,14 @@
 #include <stack>
 #include <queue>
 #include <string>
-#include "MyPoint.h"
-#include "Circle2D.h"
-#include "Rectangle2D.h"
 using namespace std;
 
 /*
-Q1. GeometricObject 베이스 클래스로 해서
-	다음 클래스들을 설계하라.
-	Circle2D, Rectangle2D, MyPoint 클래스를
-	추상 클래스 상속 개념을 적용하여 다시 작성하라.
-	UML 클래스 다이어그램 작성.
+Q1. p.889 연습 문제 2
+
+Q2. 후위표기법을 활용하여 스택 계산기 프로그램을 작성하라.
+	() < +,- < x,/
+	((4x12)x(123+7)
 */
 
 struct OPERATOR
@@ -24,50 +21,122 @@ struct OPERATOR
 
 int main()
 {
-	cout << "* MyPoint 확인 *\n";
-	MyPoint p1;
-	MyPoint p2(10, 30.5);
-	Circle2D circle(0, 0, 5.5);
-	Rectangle2D rect(10, 10, 2, 2);
+	string str;
+	stack<string> st;
+	queue<string> q;
+	OPERATOR operators[6] = { {"(", 0}, {")",0}, {"+", 1}, {"-",1}, {"x", 2}, {"/", 2} };
+	bool isOperator = false;
+	string numTemp;
+	int numTempLength = 0;
+	string prevTemp;
+	int prevNum = 0;
 
-	cout << "p1 = " << p1 << '\n' << "p2 = " << p2 << '\n';
-	cout << "circle = " << circle << '\n' << "rect = " << rect << "\n\n";
+	//str = "(2+2)x(3+3)";
+	cout << "계산식을 입력하세요: \n";
+	cin >> str;
 
-	cout << "p1과 p2 사이의 거리: " << p1.distance(p2.getX(), p2.getY()) << '\n';
-	cout << boolalpha << "contains(circle, p1): " << contains(circle, p1) << '\n';
-	cout << "contains(rect, p1): " << contains(rect, p1) << "\n\n";
+	for (int i = 0; i < str.length(); i++)
+	{
+		isOperator = false;
 
-	cout << "* Circle2D 확인 *\n";
-	Circle2D c1(2, 2, 5.5);
-	Circle2D c2(2, 2, 5.5);
-	Circle2D c3(4, 5, 10.5);
+		for (int j = 0; j < 6; j++)
+		{
+			if (i == 0 && str[i] == '-')
+				break;
 
-	cout << "c1: " << c1 << '\n';
+			if (operators[j].c[0] == str[i])
+			{
+				isOperator = true;
 
-	cout << "c2: " << c2 << '\n';
+				if (!st.empty())
+				{
+					if (st.top() == "+" || st.top() == "-")
+						prevNum = 1;
+					else if (st.top() == "x" || st.top() == "/")
+						prevNum = 2;
+				}
 
-	cout << "c3: " << c3 << '\n';
+				if (numTempLength != 0)
+				{
+					q.push(numTemp);
+					numTempLength = 0;
+					numTemp = "";
+				}
 
-	cout << "c1의 면적: " << c1.getArea() << '\n';
-	cout << "c1의 둘레: " << c1.getPerimeter() << '\n';
+				if (str[i] == '(')
+				{
+					st.push(operators[j].c);
+					break;
+				}
 
-	cout << boolalpha << "c1.isContains(3,3): " << c1.contains(3, 3) << '\n';
-	cout << "c1.isContains(c2): " << c1.contains(c2) << '\n';
-	cout << "c1.isOverlaps(c3): " << c1.overlaps(c3) << "\n\n";
+				if (str[i] == ')')
+				{
+					while (st.top() != "(")
+					{
+						q.push(st.top());
+						st.pop();
+					}
 
-	cout << "* Rectangle2D 확인 *\n";
+					st.pop();	// "(" 삭제
+					break;
+				}
 
-	Rectangle2D r1(2, 2, 5.5, 4.9);
-	Rectangle2D r2(4, 5, 10.5, 3.2);
-	Rectangle2D r3(3, 5, 2.3, 5.4);
+				// 이전 거가 우선순위 더 높다면
+				if (prevNum > operators[j].num && st.top() != "(")
+				{
+					q.push(st.top());
+					st.pop();
 
-	cout << "r1: " << r1 << '\n';
-	cout << "r2: " << r2 << '\n';
-	cout << "r3: " << r3 << '\n';
+					st.push(operators[j].c);
+				}
+				else
+				{
+					st.push(operators[j].c);
+				}
+				break;
+			}
+		}
 
-	cout << "r1의 면적: " << r1.getArea() << '\n';
-	cout << "r1의 둘레: " << r1.getPerimeter() << '\n';
-	cout << boolalpha << "r1.contains(3,3): " << r1.contains(3, 3) << '\n';
-	cout << "r1.contains(r2): " << r1.contains(r2) << '\n';
-	cout << "r1.overlaps(r3): " << r1.overlaps(r3) << '\n';
+		if (isOperator == false)	// 숫자
+		{
+			numTempLength++;
+			numTemp += str[i];
+		}
+	}
+
+	while (!st.empty())
+	{
+		q.push(st.top());
+		st.pop();
+	}
+
+	string top;
+
+	while (!q.empty())
+	{
+		st.push(q.front());
+		q.pop();
+
+		if (st.top() == "x" || st.top() == "+" || st.top() == "-" || st.top() == "/")
+		{
+			top = st.top();
+
+			st.pop();
+			int temp = stoi(st.top());
+			st.pop();
+
+			if (top == "x")
+				temp *= stoi(st.top());
+			else if (top == "+")
+				temp += stoi(st.top());
+			else if (top == "-")
+				temp -= stoi(st.top());
+			else
+				temp /= stoi(st.top());
+			st.pop();
+			st.push(to_string(temp));
+		}
+	}
+
+	cout << "결과: " << st.top() << endl;
 }
