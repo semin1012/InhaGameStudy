02fff81f053e6dfc6e5d9411c2b442348060fa15
semin1012 <semin1012@gmail.com>
2024-08-19 15:15:28 +0900

    37일차 챕터 16 예제

--- Inha/Inha/Inha.cpp
+++ Inha/Inha/Inha.cpp
@@ -1,168 +1,134 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <vector>
+#include <list>
+#include <iterator>
 #include <algorithm>
-#include <iomanip>
 #include "StopWatch.h"
 using namespace std;
 
 /*
-Q1. StopWatch class 설계
-	다음의 내용을 포함하는 클래스를 설계하라.
-	- get 함수를 갖는 데이터 필드 startTime과 endTime
-	- startTime을 현재 시간으로 초기화하는 인수 없는 생성자
-	- startTime을 현재 시간으로 재설정하는 start() 함수
-	- endTime을 현재 시간으로 설정하는 stop() 함수
-	- 스톱워치의 경과 시간을 밀리초(milliseconds)로 반환하는 
-	  getElpasedTime() 함수
-	- UML 클래스 다이어그램 작성
-*/
-
-
-#define MAX 5
-
-StopWatch watch;
-
-void Swap(int& a, int& b)
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
-
-void printArray(int arr[], int tempi, int tempj)
-{
-	for (int i = 0; i < MAX; i++)
-	{
-		if (i == tempi || i == tempj)
-			cout << setw(2) << " * ";
-		else cout << setw(2) << "   ";
-	}
-
-	cout << "\n";
-
-	for (int i = 0; i < MAX; i++)
-	{
-		cout << setw(2) << arr[i] << " ";
-	}
-	cout << "\n";
-}
-
-void printArray(int arr[])
-{
-	for (int i = 0; i < MAX; i++)
-	{
-		cout << arr[i] << " ";
-	}
-	cout << "\n";
-}
-
-void bubbleSortUpgrade(int arr[])
-{
-	int path = 1;
-	int count;
-	int change;
-	int compair = 0;
-
-	for (int i = 0; i < MAX; i++)
-	{
-		//cout << "\n패스" << path << ", 비교: " << compair << "\n";
-		count = 0;
-		for (int j = MAX - 1; j > i; j--)
-		{
-			compair++;
-			if (arr[j] < arr[j - 1])
-			{
-				Swap(arr[j], arr[j - 1]);
-				count++;
-				change = j - 1;
-			}
-			else change = -1;
-		}
-		path++;
-		if (count == 0) break;
-	}
-	//printArray(arr);
-}
-
-void insertSort(int arr[])
-{
-	int min;
-	int tempj;
-	for (int i = 0; i < MAX; i++)
-	{
-		min = arr[i];
-		tempj = i;
-		for (int j = i + 1; j < MAX; j++)
-		{
-			if (min > arr[j])
-			{
-				min = arr[j];
-				tempj = j;
-			}
-		}
-		printArray(arr, i, tempj);
-		Swap(arr[i], arr[tempj]);
-	}
-	//printArray(arr);
-}
-
-void insertSortUpgrade(int arr[])
-{
-	int min;
-	int tempj;
-	for (int i = 0; i < MAX; i++)
-	{
-		for (int j = i + 1; j < MAX; j++)
-		{
-			if (arr[i] > arr[j])
-			{
-				Swap(arr[i], arr[j]);
-				tempj = j;
-				printArray(arr, i, tempj);
-			}
-		}
-	}
-	//printArray(arr);
-}
+// 예제 16.12
+void outint(int n) { std::cout << n << " "; }
 
 int main()
 {
+	using namespace std;
+	list<int> one(5, 2);
+	int stuff[5] = { 1, 2, 4, 8, 6 };
+	list<int> two;
+	two.insert(two.begin(), stuff, stuff + 5);
+	int more[6] = { 6, 4, 2, 4, 6, 5 };
+	list<int> three(two);
+	three.insert(three.end(), more, more + 6);
 
-	int arr[MAX];
-	int arrTemp[MAX];
+	cout << "리스트 one: ";
+	for_each(one.begin(), one.end(), outint);
+	cout << endl << "리스트 two: ";
+	for_each(two.begin(), two.end(), outint);
+	cout << endl << "리스트 three: ";
+	for_each(three.begin(), three.end(), outint);
+	three.remove(2);
+	cout << endl << "리스트 three에서 2들을 삭제: ";
+	for_each(three.begin(), three.end(), outint);
+	three.splice(three.begin(), one);
+	cout << endl << "접목 후의 리스트 three: ";
+	for_each(three.begin(), three.end(), outint);
+	cout << endl << "리스트 one: ";
+	for_each(one.begin(), one.end(), outint);
+	three.unique();
+	cout << endl << "단일화 후의 리스트 three: ";
+	for_each(three.begin(), three.end(), outint);
+	three.sort();
+	three.unique();
+	cout << endl << "정렬과 단일화 후의 리스트 three: ";
+	for_each(three.begin(), three.end(), outint);
+	two.sort();
+	three.merge(two);
+	cout << endl << "정렬된 리스트 two를 리스트 three에 합병: ";
+	for_each(three.begin(), three.end(), outint);
+	cout << endl;
 
-	for (int i = 0; i < MAX; i++)
-	{
-		arr[i] = i+1;
-	}
-
-	random_shuffle(begin(arr), end(arr));
-
-	//cout << "원본 데이터:\n";
-	//printArray(arr);
-	cout << "\n\n";
-
-	copy(begin(arr), end(arr), begin(arrTemp));
-	cout << "버블 정렬(개선 1):\n";
-	watch.start();
-	bubbleSortUpgrade(arrTemp);
-	watch.stop();
-	cout << "시간: " << watch.getElpasedTime() << " ms" << endl;
-
-	watch.start();
-	cout << "\n\n기존 단순 선택 정렬:\n";
-	insertSort(arrTemp);
-	watch.stop();
-	cout << "시간: " << watch.getElpasedTime() << " ms" << endl;
-
-	copy(begin(arr), end(arr), begin(arrTemp));
-	cout << "\n\n";
-
-	cout << "개선? 단순 선택 정렬:\n";
-	watch.start();
-	insertSortUpgrade(arrTemp);
-	watch.stop();
-	cout << "시간: " << watch.getElpasedTime() << " ms" << endl;
-
+	return 0;
 }
+*/
+
+/*
+// 예제 16.13
+#include <string>
+#include <set>
+
+int main()
+{
+	using namespace std;
+	const int N = 6;
+	string s1[N] = { "buffoon", "thinkers", "for", "heavy", "can", "for" };
+	string s2[N] = { "metal", "any", "food", "elegant", "deliver", "for" };
+
+	set<string> A(s1, s1 + N);
+	set<string> B(s2, s2 + N);
+
+	ostream_iterator<string, char> out(cout, " ");
+	cout << "집합 A: ";
+	copy(A.begin(), A.end(), out);
+	cout << endl;
+	cout << "집합 B: ";
+	copy(B.begin(), B.end(), out);
+	cout << endl;
+	cout << "A와 B의 합집합:\n";
+	set_union(A.begin(), A.end(), B.begin(), B.end(), out);
+	cout << endl;
+
+	cout << "A와 B의 교집합:\n";
+	set_intersection(A.begin(), A.end(), B.begin(), B.end(), out);
+	cout << endl;
+
+	cout << "A와 B의 차집합:\n";
+	set_difference(A.begin(), A.end(), B.begin(), B.end(), out);
+	cout << endl;
+
+	set<string> C;
+	cout << "집합 C:\n";
+	set_union(A.begin(), A.end(), B.begin(), B.end(), insert_iterator<set<string>>(C, C.begin()));
+	copy(C.begin(), C.end(), out);
+	cout << endl;
+
+	string s3("grungy");
+	C.insert(s3);
+	cout << "삽입한 후의 집합 C:\n";
+	copy(C.begin(), C.end(), out);
+	cout << endl;
+
+	cout << "특정한 범위를 표시:\n";
+	copy(C.lower_bound("ghost"), C.upper_bound("spook"), out);
+	cout << endl;
+
+	return 0;
+}
+*/
+
+/*
+#include <map>
+#include <string>
+
+typedef int KeyType;
+typedef std::pair<const KeyType, std::string> Pair;
+typedef std::multimap<KeyType, std::string> MapCode;
+
+int main()
+{
+	using namespace std;
+	MapCode codes;
+
+	codes.insert(Pair(415, "샌프란시스코"));
+	codes.insert(Pair(510, "오클랜드"));
+	codes.insert(Pair(718, "브루클린"));
+	codes.insert(Pair(415, "샌라파엘"));
+	codes.insert(Pair(510, "버클리"));
+
+	cout << "지역 코드가 415인 도시 수: " << codes.count(415) << endl;
+	cout << "지역 코드가 718인 도시 수: " << codes.count(718) << endl;
+	cout << "지역 코드가 510인 도시 수: " << codes.count(510) << endl;
+	cout << "지역 코드 도시\n";
+	MapCode::iterator it;
+}
+*/
