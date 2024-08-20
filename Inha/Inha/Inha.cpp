7f12300dae3753797da783c4b93f502d96254b13
semin1012 <semin1012@gmail.com>
2024-08-20 09:57:15 +0900

    38일차 챕터 16 예제

--- Inha/Inha/Inha.cpp
+++ Inha/Inha/Inha.cpp
@@ -1,7 +1,5 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <algorithm>
-#include <iterator>
 #include "StopWatch.h"
 using namespace std;
 
@@ -11,53 +9,137 @@ Q2. 단순 삽입 정렬 구현
 
 */
 
-#define MAX 7
-
-int nums[MAX] = { 6, 4, 8, 5, 2, 9, 7 };
-
-
-void printArray(int num[], int in, int out)
+// 16-17
+/*
+template<class T>
+class TooBig
 {
-	for (int i = 0; i < MAX; i++)
-	{
-		cout << num[i] << " ";
-	}
-	cout << "\n";
-	for (int i = 0; i < MAX; i++)
-	{
-		if (i == in)
-			cout << "+";
-		else if (i == out)
-			cout << "^-";
-		else if (i < in && i > out)
-			cout << "--";
-		else cout << "  ";
-	}
+private:
+	T cutoff;
+public:
+	TooBig(const T& t) : cutoff(t) { }
+	bool operator() (const T& v) { return v > cutoff; }
+};
 
-	cout << endl;
-
-}
-
-void insertSort()
-{
-	int j = 0;
-	int tempnums[MAX];
-
-	for (int i = 1; i < MAX; i++)
-	{
-		copy(begin(nums), end(nums), tempnums);
-		int temp = nums[i];
-		for (j = i; j > 0 && nums[j - 1] > temp; j--)
-		{
-			nums[j] = nums[j - 1];
-		}
-		printArray(tempnums, i, j);
-		nums[j] = temp;
-	}
-
-}
+void outint(int n) { std::cout << n << " "; }
 
 int main()
 {
-	insertSort();
+	using std::list;
+	using std::cout;
+	using std::endl;
+
+	TooBig<int> f100(100);
+	int vals[10] = { 50, 100, 90, 180, 60, 210, 415, 88, 188, 201 };
+	list<int> yadayada(vals, vals + 10);
+	list<int> etcetera(vals, vals + 10);
+
+	cout << "원래의 리스트:\n";
+	for_each(yadayada.begin(), yadayada.end(), outint);
+	cout << endl;
+	for_each(etcetera.begin(), etcetera.end(), outint);
+	cout << endl;
+	yadayada.remove_if(f100);
+	etcetera.remove_if(TooBig<int>(200));
+	cout << "정비된 리스트:\n";
+	for_each(yadayada.begin(), yadayada.end(), outint);
+	cout << endl;
+	for_each(etcetera.begin(), etcetera.end(), outint);
+	cout << endl;
+	return 0;
+}
+*/
+/*
+// 16-18
+void Show(int);
+const int LIM = 10;
+int main()
+{
+	using namespace std;
+	int ar[LIM] = { 4, 5, 4, 2, 2, 3, 4, 8, 1, 4 };
+	list<int> la(ar, ar + LIM);
+	list<int> lb(la);
+	cout << "오리지널 리스트의 내용:\n\t";
+	for_each(la.begin(), la.end(), Show);
+	cout << endl;
+	la.remove(4);
+	cout << "remove() 메서드를 사용한 후:\n";
+	cout << "la:\t";
+	for_each(la.begin(), la.end(), Show);
+	cout << endl;
+	list<int>::iterator last;
+	last = remove(lb.begin(), lb.end(), 4);
+	cout << "remove() 함수를 사용한 후:\n";
+	cout << "lb:\t";
+	for_each(lb.begin(), lb.end(), Show);
+	cout << endl;
+	lb.erase(last, lb.end());
+	cout << "erase() 메서드를 사용한 후:\n";
+	cout << "lb:\t";
+	for_each(lb.begin(), lb.end(), Show);
+	cout << endl;
+	return 0;
+}
+
+void Show(int v)
+{
+	std::cout << v << ' ';
+}
+*/
+
+/**/
+// 16-19
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <cctype>
+#include <list>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+char toLower(char ch) { return tolower(ch); }
+string& ToLower(string& st);
+void display(const string& s);
+
+int main()
+{
+	vector<string> words;
+	cout << "단어들을 입력하십시오 (끝내려면 quit):\n";
+	string input;
+	while (cin >> input && input != "quit")
+		words.push_back(input);
+
+	cout << "다음과 같은 단어들을 입력하셨습니다.\n";
+	for_each(words.begin(), words.end(), display);
+	cout << endl;
+
+	set<string> wordset;
+	transform(words.begin(), words.end(), insert_iterator<set<string>>(wordset, wordset.begin()), ToLower);
+	cout << "\n단어들의 알파벳순 리스트:\n";
+	for_each(wordset.begin(), wordset.end(), display);
+	cout << endl;
+
+	map<string, int> wordmap;
+	set<string>::iterator si;
+	for (si = wordset.begin(); si != wordset.end(); si++)
+		wordmap[*si] = count(words.begin(), words.end(), *si);
+
+	cout << "\n단어별 빈도:\n";
+	for (si = wordset.begin(); si != wordset.end(); si++)
+		cout << *si << ": " << wordmap[*si] << endl;
+
+	return 0;
+}
+
+string& ToLower(string& st)
+{
+	transform(st.begin(), st.end(), st.begin(), toLower);
+	return st;
+}
+
+void display(const string& s)
+{
+	cout << s << " ";
 }
