14009707e51fa90040fcaf8fd6145d81ccb7b8a6
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-08-25 19:48:34 +0900

    n-queen 문제

--- Inha/Inha/Inha.cpp
+++ Inha/Inha/Inha.cpp
@@ -1,112 +1,86 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <fstream>
 #include "StopWatch.h"
 using namespace std;
 
-/*
-Q1. Encoding
-	파일 내의 모든 바이트에 임의 수를 더해 파일을 부호화하라.
-	사용자가 입력 파일 이름과 출력 파일 이름을 입력하면
-	입력 파일의 암호화된 버전을 출력 파일로 저장하는 프로그램을
-	작성하라.
-	ex> Input source filename: source.txt
-		Input encoding filename: encoding.txt
-	encoding.txt에는 더한 임의 수를 알아볼 수 있도록 정보를 
-	기입해준다. 
+// #define MAX 10
 
-Q2. Decoding
-	암호화된 파일을 복호화하는 프로그램을 작성하라.
-	사용자가 입력 파일 이름과 출력 파일 이름을 입력하면
-	입력 파일의 암호가 풀린 버전을 출력 파일에 저장하라.
-	ex> Input source filename: encoding.txt
-		Input decoding filename: dest.txt
-	더해진 임의 수에 대한 정보를 읽어서 decoding 하도록 한다. 
-*/
+//int arr[MAX];
 
-void Encoding(int num)
+//int QuickSort(int left, int right)
+//{
+//	int i = left, j = right;
+//	int pivot = arr[i];
+//
+//	while (i != j)
+//	{
+//		while ( arr[i] )
+//		{
+//
+//		}
+//		arr[i];
+//
+//		arr[j];
+//	}
+//}
+
+
+bool isUsedDown[100];
+bool isUsedRight[100];
+bool isUsedLeft[100];
+bool board[100][100];
+int cnt = 0;
+
+void Queen(int level, int n)
 {
-	string sourceFileName;
-	string encodingFileName;
-	char temp;
-
-	cout << "Input source filename: ";
-	cin >> sourceFileName;
-
-	cout << "Input encoding filename: ";
-	cin >> encodingFileName;
-
-	ifstream inputFile(sourceFileName, ifstream::binary);
-	ofstream outfutFile(encodingFileName, ofstream::binary);
-	if (!inputFile.is_open())
+	if (level == n )
 	{
-		cout << sourceFileName << " 열기 실패\n";
-		exit(EXIT_FAILURE);
+		cnt++;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (board[i][j] == true)
+				{
+					cout << "●";
+				}
+				else cout << "○";
+			}
+			cout << endl;
+		}
+		cout << endl;
+		return;
 	}
 
-	if (!outfutFile.is_open())
+	else
 	{
-		cout << encodingFileName << " 열기 실패\n";
-		exit(EXIT_FAILURE);
+		for (int i = 0; i < n; i++)
+		{
+			if (isUsedDown[i] == true || isUsedRight[level - i + n - 1] == true || isUsedLeft[level + i] == true)
+			{
+				continue;
+			}
+				
+
+			isUsedDown[i] = true;
+			isUsedRight[level - i + n - 1] = true;
+			isUsedLeft[level + i] = true;
+
+			board[level][i] = true;
+
+			Queen(level + 1, n);
+
+			isUsedDown[i] = false;
+			isUsedRight[level - i + n - 1] = false;
+			isUsedLeft[level + i] = false;
+
+			board[level][i] = false;
+		}
 	}
-
-	// 암호화 숫자 넣기
-	outfutFile << num << '\n';
-
-	while(inputFile.read(&temp, 1))
-	{
-		temp += num;
-		outfutFile.write(&temp, 1);
-	}
-
-	cout << encodingFileName << "에 " << num << "이 더해졌습니다.\n";
-}
-
-void Decoding()
-{
-	string sourceFileName;
-	string decodingFileName;
-	char temp;
-	int num;
-	string numTemp;
-
-	cout << "Input source filename: ";
-	cin >> sourceFileName;
-
-	cout << "Input decoding filename: ";
-	cin >> decodingFileName;
-
-	ifstream inputFile(sourceFileName, ifstream::binary);
-	ofstream outfutFile(decodingFileName, ofstream::binary);
-	//inputFile.open(sourceFileName, ifstream::in | ifstream::binary);
-	if (!inputFile.is_open())
-	{
-		cout << sourceFileName << " 열기 실패\n";
-		exit(EXIT_FAILURE);
-	}
-
-	//outfutFile.open(decodingFileName, ios_base::out | ios_base::binary);
-	if (!outfutFile.is_open())
-	{
-		cout << decodingFileName << " 열기 실패\n";
-		exit(EXIT_FAILURE);
-	}
-
-	// 암호화 숫자 얻기
-	getline(inputFile, numTemp);
-	num = atoi(numTemp.c_str());
-
-	while(inputFile.read(&temp, 1))
-	{
-		temp -= num;
-		outfutFile.write(&temp, 1);
-	}
-
-	cout << sourceFileName << "이 " << decodingFileName << "에 복호화되었습니다.\n";
 }
 
 int main()
 {
-	Encoding(5);
-	Decoding();
+	Queen(0, 5);
+	cout << endl << "개수: " << cnt << endl;
 }
