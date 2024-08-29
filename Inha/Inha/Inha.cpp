149fba223b9ef096f9dd6f77601efaf576517e1d
semin1012 <semin1012@gmail.com>
2024-08-29 17:16:57 +0900

    44일차 QuickSort

--- Inha/Inha/Inha.cpp
+++ Inha/Inha/Inha.cpp
@@ -3,10 +3,10 @@
 #include <random>
 #include <time.h>
 #include "StopWatch.h"
-#define MAX 50'000
+#define MAX 500'000
 using namespace std;
 
-int nums[MAX];
+int nums[MAX] = { 1, 6, 5, 7, 9, 4, 10, 8, 2, 3 };
 
 void Swap(int& a, int& b)
 {
@@ -24,48 +24,29 @@ void PrintNums()
 	cout << "\n";
 }
 
-void Heap(int idx, int lastIdx, bool isRight)
+
+void QuickSort(int left, int right)
 {
-	if (idx > lastIdx) return;
-
-	else if (idx == lastIdx)
-	{
-		if (nums[0] < nums[lastIdx]) 
-			return;
-		Swap(nums[0], nums[lastIdx]);
-		return;
-	}
-
-	Heap(idx * 2 + 1, lastIdx, false);
-	Heap(idx * 2 + 2, lastIdx, true);
-
-	// 부모가 더 작다면 swap
-	if (nums[(idx - 1) / 2] < nums[idx])
-	{
-		if (isRight)
-		{
-			// 형제가 나보다 더 크다면 형제가 부모랑 바뀌도록
-			if (nums[idx - 1] > nums[idx])
-			{
-				Swap(nums[(idx - 1) / 2], nums[idx - 1]);
-				return;
-			}
-		}
-		Swap(nums[(idx - 1) / 2], nums[idx]);
-	}
-}
-
-void HeapSort()
-{
-	int maxIdx = 0;
-
-	for (int i = 1; i < MAX; i++)
-		if (nums[maxIdx] < nums[i]) maxIdx = i;
+	int pivot = (left + right + 1) / 2;
+	int pl = left;
+	int pr = right;
 	
-	Swap(nums[0], nums[maxIdx]);
+	//cout << pivot << ", " << nums[pivot] << endl;
+	while (pl <= pr)
+	{
+		while (nums[pl] < nums[pivot]) pl++;
+		while (nums[pr] > nums[pivot]) pr--;
+		if (pl <= pr)
+		{
+			Swap(nums[pl], nums[pr]);
+			pl++;
+			pr--;
+			//PrintNums();
+		}
+	}
 
-	for (int i = 0; i < MAX; i++)
-		Heap(0, MAX - 1 - i, false);
+	if (left < pr) QuickSort(left, pr);
+	if (right > pl) QuickSort(pl, right);
 }
 
 int main()
@@ -80,10 +61,10 @@ int main()
 	StopWatch timer;
 	timer.start();
 	//PrintNums();
-	HeapSort();
+	QuickSort(0, MAX - 1);
 	//PrintNums();
 	timer.stop();
 
-	cout << "------ 힙정렬 ------\n";
+	cout << "------ 퀵 정렬 ------\n";
 	cout << "데이터 수: " << MAX << "개, 소요 시간: " << timer.getElpasedTime() << "\n";
 }
