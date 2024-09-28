db665e05c4b6e9063468626822f68cae7bafbc58
semin1012 <semin1012@gmail.com>
2024-09-28 11:25:50 +0900

    [C#] MatchGame 프로젝트 생성 및 수정

---
+++ AI/MatchGame/MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			SetUpGame();
+		}
+
+		private void SetUpGame()
+		{
+			List<string> animalEmogi = new List<string>()
+			{
+				"👻", "👻",
+				"🐣", "🐣",
+				"🐾", "🐾",
+				"✨", "✨",
+				"🌸", "🌸",
+				"🍑", "🍑",
+				"🌼", "🌼",
+				"🌈", "🌈"
+			};
+
+			Random random = new Random();
+			foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+			{
+				textBlock.Visibility = Visibility.Visible;
+				int index = random.Next(animalEmogi.Count);
+				string nextemogi = animalEmogi[index];
+				textBlock.Text = nextemogi;	
+				animalEmogi.RemoveAt(index);
+			}
+		}
+	}
+}
