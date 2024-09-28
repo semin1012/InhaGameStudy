e37d151720a85779616a3219b6289523f2e35b9a
semin1012 <semin1012@gmail.com>
2024-09-28 15:27:33 +0900

    [C#] 그림 맞추기 게임

--- AI/MatchGame/MatchGame/MainWindow.xaml.cs
+++ AI/MatchGame/MatchGame/MainWindow.xaml.cs
@@ -11,15 +11,57 @@ using System.Windows.Shapes;
 
 namespace MatchGame
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
+	using System.Windows.Threading;
+
 	public partial class MainWindow : Window
 	{
+		DispatcherTimer timer = new DispatcherTimer();
+		float gameTime = 8.0f;
+		int tenthsOfSecondsElapsed;
+		int matchesFound;
+		bool bGameStart = false;
+		int bestScore;
+		int currentScore;
+
+		TextBlock lastTextBlockClicked;
+		bool findingMatch = false;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			timer.Interval = TimeSpan.FromSeconds(.1);
+			timer.Tick += Timer_Tick;
 			SetUpGame();
+		}
+
+		private void Timer_Tick(object? sender, EventArgs e)
+		{
+			if ( bGameStart)
+			{
+				tenthsOfSecondsElapsed++;
+				TimeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+
+				CurrentScoreBlock.Text = "현재 점수: " + currentScore;
+				BestTimeTextBlock.Text = "최고 기록: " + bestScore;
+
+				if ( tenthsOfSecondsElapsed / 10F >= gameTime)
+				{
+					StopGame();
+				}
+
+				if (matchesFound == 8)
+				{
+					timer.Stop();
+					TimeTextBlock.Text = TimeTextBlock.Text + " - play agian?";
+				}
+			}
+		}
+		private void click_Click(object sender, RoutedEventArgs e)
+		{
+			SetUpGame();
+			bGameStart = true;
+			startButton.Visibility = Visibility.Hidden;
 		}
 
 		private void SetUpGame()
@@ -39,11 +81,76 @@ namespace MatchGame
 			Random random = new Random();
 			foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
 			{
-				textBlock.Visibility = Visibility.Visible;
-				int index = random.Next(animalEmogi.Count);
-				string nextemogi = animalEmogi[index];
-				textBlock.Text = nextemogi;	
-				animalEmogi.RemoveAt(index);
+				if (textBlock.Name != "TimeTextBlock" && textBlock.Name != "BestTimeTextBlock" && textBlock.Name != "CurrentScoreBlock")
+				{
+					textBlock.Visibility = Visibility.Visible;
+					int index = random.Next(animalEmogi.Count);
+					string nextemogi = animalEmogi[index];
+					textBlock.Text = nextemogi;	
+					animalEmogi.RemoveAt(index);
+				}
+			}
+
+			timer.Start();
+			tenthsOfSecondsElapsed = 0;
+			matchesFound = 0;
+			TimeTextBlock.Text = "0.0s";
+			CurrentScoreBlock.Text = "현재 점수: " + currentScore;
+			BestTimeTextBlock.Text = "최고 기록: " + bestScore;
+
+		}
+
+		private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (bGameStart == false)
+				return;
+
+			TextBlock textBlock = sender as TextBlock;
+			if (findingMatch == false)
+			{
+				textBlock.Visibility = Visibility.Hidden;
+				lastTextBlockClicked = textBlock;
+				findingMatch = true;
+			}
+			else if (textBlock.Text == lastTextBlockClicked.Text)
+			{
+				currentScore += 10;
+				matchesFound++;
+				textBlock.Visibility = Visibility.Hidden;
+				findingMatch = false;
+			}
+			else
+			{
+				lastTextBlockClicked.Visibility = Visibility.Visible;
+				findingMatch = false;
+			}
+
+			if ( currentScore >= 8 * 10 )
+			{
+				if ( currentScore > bestScore )
+				{
+					bestScore = currentScore;
+				}
+				StopGame();
+			}
+		}
+
+		private void StopGame()
+		{
+			timer.Stop();
+			bGameStart = false;
+			startButton.Visibility = Visibility.Visible;
+			startButton.Content = "다시 시작";
+			BestTimeTextBlock.Text = "최고 기록: " + bestScore;
+			CurrentScoreBlock.Text = "현재 점수: " + currentScore;
+			currentScore = 0;
+		}
+
+		private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (matchesFound == 8)
+			{
+				SetUpGame();
 			}
 		}
 	}
