16097dc6a82ad11c0de2136e49652704d69a78e2
semin1012 <semin1012@gmail.com>
2024-11-11 12:05:16 +0900

    [유니티] 카메라 관련 수업 업데이트

--- InhaUnityStudyUniversal/Assets/Resources/Scripts/1111/RoomMirror.cs
+++ InhaUnityStudyUniversal/Assets/Resources/Scripts/1111/RoomMirror.cs
@@ -5,27 +5,17 @@ using UnityEngine;
 public class RoomMirror : MonoBehaviour
 {
     private Camera roomMirror;
-	public Camera _RoomMirror
-	{
-		get
-		{
-			return roomMirror;
-		}
-		set
-		{
-			roomMirror = value;
-		}
-	}
+
     void Start()
     {
-		_RoomMirror = GetComponent<Camera>();
+		roomMirror = GetComponent<Camera>();
     }
 
 	private void OnPreCull()
 	{
 		// Cull 방향을 바꿔준다.
-        _RoomMirror.ResetProjectionMatrix();
-		_RoomMirror.projectionMatrix = _RoomMirror.projectionMatrix * Matrix4x4.Scale(new Vector3(-1, 1, 1)); // x축 반전
+		roomMirror.ResetProjectionMatrix();
+		roomMirror.projectionMatrix = roomMirror.projectionMatrix * Matrix4x4.Scale(new Vector3(-1, 1, 1)); // x축 반전
 	}
 
 	private void OnPreRender()
