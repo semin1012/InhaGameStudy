7c7af25c4a26ad1adaf1e51e997d50304f245c13
semin1012 <semin1012@gmail.com>
2024-11-12 12:33:07 +0900

    Update PipeSpawner.cs

--- InhaUnityStudyUniversal/Assets/Resources/Scripts/1112/PipeSpawner.cs
+++ InhaUnityStudyUniversal/Assets/Resources/Scripts/1112/PipeSpawner.cs
@@ -11,8 +11,6 @@ public class PipeSpawner : MonoBehaviour
 	private void Start()
 	{
 		StartCoroutine("SpawnPipe");
-        // Invoke("SpawnPipe2", 1.0f); // 한번만 실행 
-        //InvokeRepeating("SpawnPipe2", 1.0f, 1.0f);  // 반복 실행 Name, Delay Time, Repeat Time
 	}
 
 	IEnumerator SpawnPipe()
@@ -22,10 +20,5 @@ public class PipeSpawner : MonoBehaviour
             yield return new WaitForSeconds(interval);            
             Instantiate(pipePrefab, new Vector3(transform.position.x, Random.Range(-range, range), transform.position.z), transform.rotation);
         }
-    }
-
-    void SpawnPipe2()
-    {
-        Debug.Log("<color=red>Spawn Pipe</color>");
     }
 }
